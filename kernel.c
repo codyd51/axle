d3c091de292110474cbbd4445c6ca4cf8a89cbf1
Phillip Tennen <phillip.ennen@gmail.com>
2016-04-17 20:44:19 -0600

    Add newline support to character rendering

--- kernel.c
+++ kernel.c
@@ -83,7 +83,14 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
 }
 
 void terminal_putchar(char c) {
-    terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+    //check for newline character
+    if (c == '\n') {
+        terminal_row++;
+        terminal_column = 0;
+    }
+    else {
+        terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+    }
     if (++terminal_column == VGA_WIDTH) {
         terminal_column = 0;
         if (++terminal_row == VGA_HEIGHT) {
