d7a44ebc1fd07d2e38def47cbfc84d7404409d8b
Phillip Tennen <phillip.ennen@gmail.com>
2016-04-17 22:22:36 -0600

    Add test code for outputting text

--- kernel.c
+++ kernel.c
@@ -16,40 +16,40 @@
 
 //hardware text mode color constants
 enum vga_color {
-    COLOR_BLACK = 0,
-    COLOR_BLUE = 1,
-    COLOR_GREEN = 2,
-    COLOR_CYAN = 3,
-    COLOR_RED = 4,
-    COLOR_MAGENTA = 5,
-    COLOR_BROWN = 6,
-    COLOR_LIGHT_GREY = 7,
-    COLOR_DARK_GREY = 8,
-    COLOR_LIGHT_BLUE = 9,
-    COLOR_LIGHT_GREEN = 10,
-    COLOR_LIGHT_CYAN = 11,
-    COLOR_LIGHT_RED = 12,
-    COLOR_LIGHT_MAGENTA = 13,
-    COLOR_LIGHT_BROWN = 14,
-    COLOR_WHITE = 15,
+	COLOR_BLACK = 0,
+	COLOR_BLUE = 1,
+	COLOR_GREEN = 2,
+	COLOR_CYAN = 3,
+	COLOR_RED = 4,
+	COLOR_MAGENTA = 5,
+	COLOR_BROWN = 6,
+	COLOR_LIGHT_GREY = 7,
+	COLOR_DARK_GREY = 8,
+	COLOR_LIGHT_BLUE = 9,
+	COLOR_LIGHT_GREEN = 10,
+	COLOR_LIGHT_CYAN = 11,
+	COLOR_LIGHT_RED = 12,
+	COLOR_LIGHT_MAGENTA = 13,
+	COLOR_LIGHT_BROWN = 14,
+	COLOR_WHITE = 15,
 };
 
 uint8_t make_color(enum vga_color fg, enum vga_color bg) {
-    return fg | bg << 4;
+	return fg | bg << 4;
 }
 
 uint16_t make_vgaentry(char c, uint8_t color) {
-    uint16_t c16 = c;
-    uint16_t color16 = color;
-    return c16 | color16 << 8;
+	uint16_t c16 = c;
+	uint16_t color16 = color;
+	return c16 | color16 << 8;
 }
 
 size_t strlen(const char* str) {
-    size_t ret = 0;
-    while (str[ret] != 0) {
-        ret++;
-    }
-    return ret;
+	size_t ret = 0;
+	while (str[ret] != 0) {
+		ret++;
+	}
+	return ret;
 }
 
 static const size_t VGA_WIDTH = 80;
@@ -61,49 +61,74 @@ uint8_t terminal_color;
 uint16_t* terminal_buffer;
 
 void terminal_initialize() {
-    terminal_row = 0;
-    terminal_column = 0;
-    terminal_color = make_color(COLOR_LIGHT_GREY, COLOR_BLACK);
-    terminal_buffer = (uint16_t*) 0xB8000;
-    for (size_t y = 0; y < VGA_HEIGHT; y++) {
-        for (size_t x = 0; x < VGA_WIDTH; x++) {
-            const size_t index = y * VGA_WIDTH + x;
-            terminal_buffer[index] = make_vgaentry(' ', terminal_color);
-        }
-    }
+	terminal_row = 0;
+	terminal_column = 0;
+	terminal_color = make_color(COLOR_RED, COLOR_BLACK);
+	terminal_buffer = (uint16_t*) 0xB8000;
+	for (size_t y = 0; y < VGA_HEIGHT; y++) {
+		for (size_t x = 0; x < VGA_WIDTH; x++) {
+			const size_t index = y * VGA_WIDTH + x;
+			terminal_buffer[index] = make_vgaentry(' ', terminal_color);
+		}
+	}
 }
 
 void terminal_setcolor(uint8_t color) {
-    terminal_color = color;
+	terminal_color = color;
 }
 
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
-    const size_t index = y * VGA_WIDTH + x;
-    terminal_buffer[index] = make_vgaentry(c, color);
+	const size_t index = y * VGA_WIDTH + x;
+	terminal_buffer[index] = make_vgaentry(c, color);
 }
 
 void terminal_putchar(char c) {
-    //check for newline character
-    if (c == '\n') {
-        terminal_row++;
-        terminal_column = 0;
-    }
-    else {
-        terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-    }
-    if (++terminal_column == VGA_WIDTH) {
-        terminal_column = 0;
-        if (++terminal_row == VGA_HEIGHT) {
-            terminal_row = 0;
-        }
-    }
+	//check for newline character
+	if (c == '\n') {
+	    terminal_row++;
+	    terminal_column = 0;
+	}
+	else {
+		terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+	}
+
+	if (++terminal_column == VGA_WIDTH) {
+		terminal_column = 0;
+		if (++terminal_row == VGA_HEIGHT) {	
+			terminal_row = 0;
+		}
+	}
 }
 
 void terminal_writestring(const char* data) {
-    size_t datalen = strlen(data);
-    for (size_t i = 0; i < datalen; i++) {
-        terminal_putchar(data[i]);
-    }
+	size_t datalen = strlen(data);
+	for (size_t i = 0; i < datalen; i++) {
+		terminal_putchar(data[i]);
+	}
+}
+
+char* itoa(int i, char b[]) {
+	char const digit[] = "0123456789";
+	char* p = b;
+	if (i < 0) {
+		*p++ = '-';
+		i *= -1;
+	}
+	int shifter = i;
+	do {
+		//move to where representation ends
+		++p;
+		shifter = shifter/10;
+	} while(shifter);
+	
+	*p = '\0';
+	
+	do {
+		//move back, inserting digits as we go
+		*--p = digit[i%10];
+		i = i/10;
+	} while (i);
+	return b;
 }
 
 #if defined(__cplusplus)
@@ -111,12 +136,19 @@ extern "C" //use C linkage for kernel_main
 #endif
 
 void kernel_main() {
-    //initialize terminal interface
-    terminal_initialize();
-    
-    //since there's no support for newlines in terminal_putchar (yet),
-    //\n will produce some VGA specific character instead
-    terminal_writestring("Hello world! This is a test.\n");
+	//initialize terminal interface
+	terminal_initialize();
+	
+	//terminal_writestring("Hello world! This is a test.\nTest 2.\nOne more line\nAnd another\n");
+	int upper_bound = 300;
+	for (int i = 0; i <= upper_bound; i++) {
+		char str[4];
+		terminal_writestring("iteration ");
+		terminal_writestring(itoa(i, str));
+		terminal_writestring(".\n");
+		terminal_writestring(itoa((upper_bound - i), str));
+		terminal_writestring(" iterations to go.\n");
+	}
 }
 
 
