f73eb0ad2a18cbec5f6ff0dba2fc5983453bff46
Phillip Tennen <phillip.ennen@gmail.com>
2016-04-17 20:30:57 -0600

    Initial commit

---
+++ boot.s
@@ -0,0 +1,61 @@
+#Declare constants used for creating multiboot header
+.set ALIGN,     1<<0                #align loaded modules on page boundaries
+.set MEMINFO,   1<<1                #provide memory map
+.set FLAGS,     ALIGN | MEMINFO     #multiboot 'flag' field
+.set MAGIC,     0x1BADB002          #'magic number' lets bootloader find header
+.set CHECKSUM,  -(MAGIC+FLAGS)      # checksum of above, to prove we're multiboot
+
+#declare header as in multiboot standard. Put this into a special section
+#so we can force the header to be in the start of the final program.
+#again, these are just magic values from multiboot standard, nothing special going on
+#bootloader will search for this magic sequence and recognize a multiboot kernel
+.section .multiboot
+.align 4
+.long MAGIC
+.long FLAGS
+.long CHECKSUM
+
+#currently the stack pointer reg (esp) points to anything and shoudl not be used
+#so, let's provide our own stack
+#we'll allocate room for a small temporary stack by creating a symbol at the bottom of it,
+#then allocating 16384 bytes for it, and finally creating a symbol at the top
+.section .bootstrap_stack, "aw", @nobits
+stack_bottom:
+.skip 16384 #16kb
+stack_top:
+
+#linker script specifies _start as the entry point to the kernal, and
+#the bootloader will jump to this position once the kernel has been loaded.
+#it doesn't make sense to return from this function as the bootloader is gone
+.section .text
+.global _start
+.type _start, @function
+_start:
+    #welcome to kernel mode! We now have sufficient code for the bootloader to
+    #load and run our operating system. It doesn't do much yet, though.
+
+    #to set up a stack so that we can at least use C, let's set the esp register to
+    #point to the top of our stack (grows downwards)
+    movl $stack_top, %esp
+
+    #we can now actually execute C code! A function kernel_main will be used as the entry
+    #point to the OS
+    call kernel_main
+
+    #in case that function returns, we want to put the computer into an infinite loop
+    #to do so, use clear interrupt instruction to disable interrupts,
+    #the halt instruction to stop the CPU until the next interrupt arrives,
+    #and jumping to the halt instruction if it ever continues execution, just to be safe.
+    #let's create a local label rather than a real symbol and jump to there endlessly
+    cli
+    hlt
+.Lhang:
+    jmp .Lhang
+
+#set size of the _start symbol to the current location '.' minus its start
+#useful when debugging or whenever we implement call tracing
+.size _start, . -_start
+
+
+
+
---
+++ kernel.c
@@ -0,0 +1,131 @@
+#if !defined(__cplusplus)
+#include <stdbool.h> //C doesn't have boolean type by default
+#endif
+#include <stddef.h>
+#include <stdint.h>
+
+//check if the compiler thinks we're targeting the wrong OS
+#if defined(__linux__)
+#error "You are not using a cross compiler! You will certainly run into trouble."
+#endif
+
+//OS only works for the 32-bit ix86 target
+#if !defined(__i386__)
+#error "OS must be compiled with a ix86-elf compiler."
+#endif
+
+//hardware text mode color constants
+enum vga_color {
+    COLOR_BLACK = 0,
+    COLOR_BLUE = 1,
+    COLOR_GREEN = 2,
+    COLOR_CYAN = 3,
+    COLOR_RED = 4,
+    COLOR_MAGENTA = 5,
+    COLOR_BROWN = 6,
+    COLOR_LIGHT_GREY = 7,
+    COLOR_DARK_GREY = 8,
+    COLOR_LIGHT_BLUE = 9,
+    COLOR_LIGHT_GREEN = 10,
+    COLOR_LIGHT_CYAN = 11,
+    COLOR_LIGHT_RED = 12,
+    COLOR_LIGHT_MAGENTA = 13,
+    COLOR_LIGHT_BROWN = 14,
+    COLOR_WHITE = 15,
+};
+
+uint8_t make_color(enum vga_color fg, enum vga_color bg) {
+    return fg | bg << 4;
+}
+
+uint16_t make_vgaentry(char c, uint8_t color) {
+    uint16_t c16 = c;
+    uint16_t color16 = color;
+    return c16 | color16 << 8;
+}
+
+size_t strlen(const char* str) {
+    size_t ret = 0;
+    while (str[ret] != 0) {
+        ret++;
+    }
+    return ret;
+}
+
+static const size_t VGA_WIDTH = 80;
+static const size_t VGA_HEIGHT = 25;
+
+size_t terminal_row;
+size_t terminal_column;
+uint8_t terminal_color;
+uint16_t* terminal_buffer;
+
+void terminal_initialize() {
+    terminal_row = 0;
+    terminal_column = 0;
+    terminal_color = make_color(COLOR_LIGHT_GREY, COLOR_BLACK);
+    terminal_buffer = (uint16_t*) 0xB8000;
+    for (size_t y = 0; y < VGA_HEIGHT; y++) {
+        for (size_t x = 0; x < VGA_WIDTH; x++) {
+            const size_t index = y * VGA_WIDTH + x;
+            terminal_buffer[index] = make_vgaentry(' ', terminal_color);
+        }
+    }
+}
+
+void terminal_setcolor(uint8_t color) {
+    terminal_color = color;
+}
+
+void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
+    const size_t index = y * VGA_WIDTH + x;
+    terminal_buffer[index] = make_vgaentry(c, color);
+}
+
+void terminal_putchar(char c) {
+    terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+    if (++terminal_column == VGA_WIDTH) {
+        terminal_column = 0;
+        if (++terminal_row == VGA_HEIGHT) {
+            terminal_row = 0;
+        }
+    }
+}
+
+void terminal_writestring(const char* data) {
+    size_t datalen = strlen(data);
+    for (size_t i = 0; i < datalen; i++) {
+        terminal_putchar(data[i]);
+    }
+}
+
+#if defined(__cplusplus)
+extern "C" //use C linkage for kernel_main
+#endif
+
+void kernel_main() {
+    //initialize terminal interface
+    terminal_initialize();
+    
+    //since there's no support for newlines in terminal_putchar (yet),
+    //\n will produce some VGA specific character instead
+    terminal_writestring("Hello world! This is a test.\n");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
