319096ca8ff5ffbfabc37702c794c95bf6d01527
Phillip Tennen <phillip.ennen@gmail.com>
2016-05-06 10:57:21 -0600

    Standardize types, add inw function

--- common.c
+++ common.c
@@ -1,12 +1,19 @@
 #include "common.h"
 
-void outb(unsigned short port, unsigned char val) {
+//write byte out to specified port
+void outb(u16int port, u8int val) {
 	 asm volatile("outb %0, %1" : : "a"(val), "Nd"(port) );
 }
 
-unsigned char inb(unsigned short int port) {
-	unsigned char _v;
+u8int inb(u16int port) {
+	u8int _v;
 
 	__asm__ __volatile__ ("inb %w1,%0":"=a" (_v):"Nd" (port));
 	return _v;
+}
+
+u16int inw(u16int port) {
+	u16int _v;
+
+	__asm__ __volatile__ ("inw %1, %0" : "=a" (ret) : "dN" (port));
 }
