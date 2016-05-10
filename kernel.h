b466da1773c18f0e7aaf21900e1b68694fe6c322
Phillip Tennen <phillip.ennen@gmail.com>
2016-05-10 09:05:06 -0600

    Remove kprintf in favor of printf_dbg

--- kernel.h
+++ kernel.h
@@ -29,4 +29,3 @@ void terminal_writestring(const char* data);
 void terminal_removechar();
 void terminal_clear();
 void terminal_settextcolor(enum vga_color col); 
-void kprintf(char* format, ...);
