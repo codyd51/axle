8543558dda49d277ab248132ada4d9989734bdd2
Phillip Tennen <phillip.ennen@gmail.com>
2016-05-17 12:15:42 -0600

    Add structures to represent basic shapes

--- src/gfx/lib/gfx.c
+++ src/gfx/lib/gfx.c
@@ -70,14 +70,57 @@ void vline_slow(screen_t* screen, int x, int y, int h, int color) {
 	}
 }
 
+coordinate create_coordinate(int x, int y) {
+	coordinate coord;
+	coord.x = x;
+	coord.y = y;
+	return coord;
+}
+
+size create_size(int w, int h) {
+	size size;
+	size.w = w;
+	size.h = h;
+	return size;
+}
+
+rect create_rect(coordinate origin, size size) {
+	rect rect;
+	rect.origin = origin;
+	rect.size = size;
+	return rect;
+}
+
+line create_line(coordinate p1, coordinate p2) {
+	line line;
+	line.p1 = p1;
+	line.p2 = p2;
+	return line;
+}
+
+circle create_circle(coordinate center, int radius) {
+	circle circle;
+	circle.center = center;
+	circle.radius = radius;
+	return triangle;
+}
+
+triangle create_triangle(coordinate p1, coordinate p2, coordinate p3) {
+	triangle triangle;
+	triangle.p1 = p1;
+	triangle.p2 = p2;
+	triangle.p3 = p3;
+	return triangle;
+}
+
 void gfx_test() {
 	screen_t* screen = get_vga_screen();
 	fill_screen(screen, 6);
 
-	coordinate* origin = create_coordinate(50, 50);
-	size* size = create_size(50, 50);
-	rect* rect = create_rect(origin, size);
-	draw_rect(screen, rect, 7);
+	coordinate origin = create_coordinate(50, 50);
+	size size = create_size(50, 50);
+	rect rect = create_rect(origin, size);
+	//draw_rect(screen, rect, 7);
 
 	//fillrect(screen, 50, 5, 100, 10, 6);
 	//hline_slow(screen, 80, 30, 200, 7);
--- src/gfx/lib/gfx.h
+++ src/gfx/lib/gfx.h
@@ -16,6 +16,37 @@ typedef struct {
 	uint16_t pixelwidth;
 } screen_t;
 
+typedef struct coordinate {
+	int x;
+	int y;
+} coordinate;
+
+typedef struct size {
+	int w;
+	int h;
+} size;
+
+typedef struct rect {
+	coordinate* origin;
+	size* size;
+} rect;
+
+typedef struct line {
+	coordinate* p1;
+	coordinate* p2;
+} line;
+
+typedef struct circle {
+	coordinate center;
+	int radius;
+} circle;
+
+typedef struct triangle {
+	coordinate p1;
+	coordinate p2;
+	coordinate p3;
+} triangle;
+
 extern void int32(unsigned char intnum, regs16_t* regs);
 
 screen_t* get_gfx_screen();
