95dda8dfeb7baef4fe4f230936d46d5ffcb81cab
Phillip Tennen <phillip.ennen@gmail.com>
2016-05-17 09:49:23 -0600

    Add outw function

--- src/std/common.c
+++ src/std/common.c
@@ -5,6 +5,10 @@ void outb(uint16_t port, uint8_t val) {
 	 asm volatile("outb %0, %1" : : "a"(val), "Nd"(port) );
 }
 
+void outw(uint16_t port, uint16_t val) {
+	asm volatile("outb %0, %1" : : "a"(val), "dN"(port));
+}
+
 uint8_t inb(uint16_t port) {
 	uint8_t _v;
 
