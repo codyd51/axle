f5996cade5efd097aa484fa6597e09635fcffbf1
Phillip Tennen <phillip.ennen@gmail.com>
2016-05-17 15:22:15 -0600

    Implement arccos and arcsin

--- src/std/math.h
+++ src/std/math.h
@@ -5,5 +5,8 @@ double pow(double x, double pow);
 unsigned long factorial(unsigned long x);
 
 double sin(double val);
+double arcsin(double val);
 double cos(double val);
+double arccos(double val);
+
 #endif
