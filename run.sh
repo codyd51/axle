85b4154e088e85cb6abbb2067bcccbe5f6fe9fd4
Ninjaprawn <george.r.dan@gmail.com>
2016-05-19 07:57:43 +1000

    Added xorriso command (need to confirm with phil about it)

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 make
 cp axle.bin isodir/boot/axle.bin
 cp src/boot/grub.cfg isodir/boot/grub/grub.cfg
-grub-mkrescue -o axle.iso isodir
+grub-mkrescue --xorriso=/Volumes/Files/Developer/opt/cross/bin/i686-elf-xorriso -o axle.iso isodir
 
 qemu-system-i386 -vga std -cdrom axle.iso
