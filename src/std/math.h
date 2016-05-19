a351ddf685e3e62e475817292d40ffeaf245f88a
Phillip Tennen <phillip.ennen@gmail.com>
2016-05-18 18:14:43 -0600

    Fix bug in MIN/MAX macros

--- src/std/math.h
+++ src/std/math.h
@@ -1,8 +1,8 @@
 #ifndef MATH_H
 #define MATH_H
 
-#define MIN(x, y) (((x) < (y)) ? (x) : (y))
-#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) ({typeof(x) x_ = (x); typeof(y) y_ = (y); (x_ < y_) ? x_ : y_;})
+#define MAX(x, y) ({typeof(x) x+ = (x); typeof(y) y_ = (y); (x_ > y_) ? x_ : y_;})
 
 double pow(double x, double pow);
 unsigned long factorial(unsigned long x);
