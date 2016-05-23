e8b5ec029358d38a8cdc1941f3b21a7e77661519
Phillip Tennen <phillip.ennen@gmail.com>
2016-05-23 15:39:18 -0600

    Set default background & border colors

--- src/gfx/lib/view.c
+++ src/gfx/lib/view.c
@@ -5,6 +5,7 @@
 View* create_view(Rect frame) {
 	View* view = kmalloc(sizeof(View));
 	view->frame = frame;
+	view->background_color = 0x00FF00;
 	view->subviews = create_mutable_array(16);
 	return view;
 }
@@ -13,6 +14,7 @@ Window* create_window(Rect frame) {
 	Window* window = kmalloc(sizeof(Window));
 	window->size = frame.size;
 	window->frame = frame;
+	window->border_color = 0x0000FF;
 	window->subwindows = create_mutable_array(16);
 		
 	return window;
@@ -20,7 +22,7 @@ Window* create_window(Rect frame) {
 
 void add_subview(View* view, View* subview) {
 	insert_mutable_array(subview, &(view->subviews));
-	subview->superview = view;
+	//subview->superview = view;
 }
 
 void add_subwindow(Window* window, Window* subwindow) {
