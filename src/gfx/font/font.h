b20bf884538bd9a92d411f15f5e9703e6a724260
Phillip Tennen <phillip.ennen@gmail.com>
2016-05-24 11:43:13 -0600

    Only use a single large memory block for font map

--- src/gfx/font/font.h
+++ src/gfx/font/font.h
@@ -3,18 +3,16 @@
 
 #include <gfx/lib/gfx.h>
 #include <std/common.h>
-typedef struct {
-	int rows[8];
-} char_t;
+
+#define FONT8_SIZE 128*8
 
 typedef struct {
-	char_t* characters[25];
+	uint8_t* characters[FONT8_SIZE];
 } Font;
 
 int char_index(char ch);
 
 Font* setup_font();
-int is_bit_set(int c, int n);
 void draw_char(Screen* screen, Font* font_map, char ch, int x, int y, int color);
 void draw_string(Screen* screen, Font* font_map, char* str, int x, int y, int color);
 
