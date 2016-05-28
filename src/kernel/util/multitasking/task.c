0e79f71d7f44345a8067cda6f4f9918cc2b98e0c
Phillip Tennen <phillip.ennen@gmail.com>
2016-05-28 17:39:12 -0600

    Add function to set up stack to switch to user mode

--- src/kernel/util/multitasking/task.c
+++ src/kernel/util/multitasking/task.c
@@ -217,3 +217,27 @@ void move_stack(void* new_stack_start, uint32_t size) {
 int getpid() {
 	return current_task->id;
 }
+
+void switch_to_user_mode() {
+	//set up kernel stack
+	set_kernel_stack(current_task->kernel_stack + KERNEL_STACK+SIZE);
+
+	//set up stack to switch to user mode
+	asm volatile("		\
+		cli;		\
+		mov $0x23, %ax;	\
+		mov %ax, %ds;	\
+		mov %ax, %es;	\
+		mov %ax, %fs;	\
+		mov %ax, %gs;	\
+				\
+		\
+		mov %esp, %eax;	\
+		pushl $0x23;	\
+		pushl %esp;	\
+		pushf;		\
+		pushl $0x1B;	\
+		push $1f;	\
+		iret;		\
+	    1: 	\
+	    	");
