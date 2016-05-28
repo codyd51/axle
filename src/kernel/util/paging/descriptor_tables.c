1a02f48ee7ac47716cf66f3f11af7df7e90eb07b
Phillip Tennen <phillip.ennen@gmail.com>
2016-05-28 17:50:55 -0600

    Add function to write TSS structure

--- src/kernel/util/paging/descriptor_tables.c
+++ src/kernel/util/paging/descriptor_tables.c
@@ -4,17 +4,20 @@
 //access ASM functions from C
 extern void gdt_flush(uint32_t);
 extern void idt_flush(uint32_t);
+extern void tss_flush();
 
 //internal function prototypes
 static void init_gdt();
 static void gdt_set_gate(int32_t, uint32_t, uint32_t, uint8_t, uint8_t);
 static void init_idt();
 static void idt_set_gate(uint8_t, uint32_t, uint16_t, uint8_t);
+static void write_tss(int32_t, uint16_t, uint32_t);
 
 gdt_entry_t gdt_entries[5];
 gdt_ptr_t   gdt_ptr;
 idt_entry_t idt_entries[256];
 idt_ptr_t   idt_ptr;
+tss_entry_t tss_entry;
 
 //zeroes all the interrupt service routines
 //initializes GDT and IDT
@@ -34,6 +37,7 @@ static void init_gdt() {
 	gdt_set_gate(4, 0, 0xFFFFFFFF, 0xF2, 0xCF);	//user mode data segment
 
 	gdt_flush((uint32_t)&gdt_ptr);
+	tss_flush();
 }
 
 static void init_idt() {
@@ -140,3 +144,33 @@ static void gdt_set_gate(int32_t num, uint32_t base, uint32_t limit, uint8_t acc
 	gdt_entries[num].access 	= access;
 }
 
+//initialize task state segment structure
+static void write_tss(int32_t num, uint16_t ss0, uint32_t esp0) {
+	//compute base and limit of GDT entry
+	uint32_t base = (uint32_t)&tss_entry;
+	uint32_t limit = base + sizeof(tss_entry);
+
+	//add TSS descriptor's address to GDT
+	gdt_set_gate(num, base, limit, 0xE9, 0x00);
+
+	//ensure descriptor is empty
+	memset(&tss_entry, 0, sizeof(tss_entry));
+
+	//set kernel stack segment
+	tss_entry.ss0 = ss0;
+	//set kernel stack pointer
+	tss_entry.esp0 = esp0; 
+
+	//set cs, ss, ds, es, fs, and gs entries in TSS
+	//specify what segments should be loaded when processor switches to kernel mode
+	//therefore, these are just normal kernel code/data segments: 0x08 and 0x10
+	//but, with the last two bits set, making them 0x0b and 0x13
+	//this sets the requested privilege level to 3, so this TSS can be used to switch
+	//from kernel mode to ring3
+	tss_entry.cs = 0x0b;
+	tss_entry.ss = tss_entry.ds = tss_entry.es = tss_entry.fs = tss_entry.gs = 0x13;
+}
+
+void set_kernel_stack(uint32_t stack) {
+	tss_entry.esp0 = stack;
+}
