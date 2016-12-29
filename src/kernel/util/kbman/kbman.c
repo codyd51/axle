761e11fe0a77ac63c352462667ee5c2ff4db9327
Phillip Tennen <phillip.ennen@gmail.com>
2016-12-28 22:50:46 -0700

    Output dynamic memory usage across source files to syslog on ctrl+m

--- src/kernel/util/kbman/kbman.c
+++ src/kernel/util/kbman/kbman.c
@@ -2,6 +2,8 @@
 #include <kernel/drivers/terminal/terminal.h>
 #include <std/array_m.h>
 #include <kernel/util/multitasking/tasks/task.h>
+#include <std/kheap.h>
+#include <kernel/drivers/kb/kb.h>
 
 #define MAX_KB_BUFFER_LENGTH 64
 
@@ -17,15 +19,25 @@ void kbman_process(char c) {
 	if (!key_down(c)) {
 		array_m_insert(keys_down, (type_t)c);
 	}
+	
+	key_status_t mods = kb_modifiers();
 
-	//dispatch key if necessary
-	switch (c) {
-		default:
-			break;
+	//least significant bit of mods mask is control key
+	//dispatch any ctrl+key keystrokes
+	if (mods & 0x1) {
+		switch (c) {
+			//dump dynamic memory users whenever ctrl+m is pressed
+			case 'm':
+				memdebug();
+				break;
+			default:
+				break;
+		}
 	}
 
 	//inform scheduler that a keystroke has been recieved
 	//this call forces any tasks waiting on a keystroke to be woken
+	//TODO this should take an arg for reason for waking tasks
 	force_enumerate_blocked();
 }
 
