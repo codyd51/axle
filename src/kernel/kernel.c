63e178bbae231341226601843368131f37d27f05
Phillip Tennen <pt@datatheorem.com>
2018-01-01 18:15:14 -0500

    add assertion helpers

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,10 +1,26 @@
+//freestanding headers
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 
+//kernel stdlib headers
+#include <std/printf.h>
+#include <std/string.h>
+
+//kernel headers
 #include <kernel/drivers/vga_screen/vga_screen.h>
 #include <kernel/multiboot.h>
-#include <std/printf.h>
+#include <kernel/boot.h>
+
+#define NotImplemented() do {_assert("Not implemented", __FILE__, __LINE__);} while(0);
+#define assert(msg) do {_assert(msg, __FILE__, __LINE__);} while(0);
+
+void _assert(const char* msg, const char* file, int line) {
+	printf("Kernel assertion. %s, line %d: %s\n", file, line, msg);
+	asm("cli");
+	while (1) {}
+}
+
 
 size_t strlen(const char* str) {
 	size_t len = 0;
