a36d5a2e9df9dcc629124b8857254edc720af52d
Phillip Tennen <pt@datatheorem.com>
2018-01-01 18:14:49 -0500

    reimplement snprintf, implement printf

--- src/std/printf.c
+++ src/std/printf.c
@@ -3,87 +3,259 @@
 #include <limits.h>
 
 #include <kernel/drivers/vga_screen/vga_screen.h>
+#include <std/string.h>
 
-static bool print(const char* data, size_t length) {
-	const unsigned char* bytes = (const unsigned char*) data;
-	for (size_t i = 0; i < length; i++) {
-		vga_screen_putchar(bytes[i]);
+int printf(const char* fmt, ...);
+
+static int normalize(double *val) {
+	int exponent = 0;
+	double value = *val;
+
+	while (value >= 1.0) {
+		value /= 10.0;
+		++exponent;
 	}
-	return true;
+
+	while (value < 0.1) {
+		value *= 10.0;
+		--exponent;
+	}
+	*val = value;
+	return exponent;
 }
 
-int printf(const char* restrict format, ...) {
-	va_list parameters;
-	va_start(parameters, format);
+static void ftoa_fixed(char *buffer, double value) {  
+    /* carry out a fixed conversion of a double value to a string, with a precision of 5 decimal digits. 
+     * Values with absolute values less than 0.000001 are rounded to 0.0
+     * Note: this blindly assumes that the buffer will be large enough to hold the largest possible result.
+     * The largest value we expect is an IEEE 754 double precision real, with maximum magnitude of approximately
+     * e+308. The C standard requires an implementation to allow a single conversion to produce up to 512 
+     * characters, so that's what we really expect as the buffer size.     
+     */
 
-	int written = 0;
+    int exponent = 0;
+    int places = 0;
+    static const int width = 4;
 
-	while (*format != '\0') {
-		size_t maxrem = INT_MAX - written;
+    if (value == 0.0) {
+        buffer[0] = '0';
+        buffer[1] = '\0';
+    }         
 
-		if (format[0] != '%' || format[1] == '%') {
-			//handle %%
-			if (format[0] == '%') {
-				format++;
-			}
-			size_t amount = 1;
-			while (format[amount] && format[amount] != '%') {
-				amount++;
-			}
-			if (maxrem < amount) {
-				// TODO: Set errno to EOVERFLOW.
-				return -1;
-			}
-			if (!print(format, amount)) {
-				return -1;
-			}
-			format += amount;
-			written += amount;
-			continue;
+    if (value < 0.0) {
+        *buffer++ = '-';
+        value = -value;
+    }
+
+    exponent = normalize(&value);
+
+    while (exponent > 0) {
+        int digit = value * 10;
+        *buffer++ = digit + '0';
+        value = value * 10 - digit;
+        ++places;
+        --exponent;
+    }
+
+    if (places == 0) {
+        *buffer++ = '0';
+	}
+
+    *buffer++ = '.';
+
+    while (exponent < 0 && places < width) {
+        *buffer++ = '0';
+        --exponent;
+        ++places;
+    }
+
+    while (places < width) {
+        int digit = value * 10.0;
+        *buffer++ = digit + '0';
+        value = value * 10.0 - digit;
+        ++places;
+    }
+    *buffer = '\0';
+}
+
+static unsigned int itoa_advanced(int value, unsigned int radix, unsigned int uppercase, unsigned int unsig,
+	 char *buffer, unsigned int zero_pad)
+{
+	char	*pbuffer = buffer;
+	int	negative = 0;
+	unsigned int	i, len;
+
+	/* No support for unusual radixes. */
+	if (radix > 16) {
+		return 0;
+	}
+
+	if (value < 0 && !unsig) {
+		negative = 1;
+		value = -value;
+	}
+
+	/* This builds the string back to front ... */
+	do {
+		int digit = value % radix;
+		*(pbuffer++) = (digit < 10 ? '0' + digit : (uppercase ? 'A' : 'a') + digit - 10);
+		value /= radix;
+	} while (value > 0);
+
+	for (i = (pbuffer - buffer); i < zero_pad; i++) {
+		*(pbuffer++) = '0';
+	}
+
+	if (negative) {
+		*(pbuffer++) = '-';
+	}
+
+	*(pbuffer) = '\0';
+
+	/* ... now we reverse it (could do it recursively but will
+	 * conserve the stack space) */
+	len = (pbuffer - buffer);
+	for (i = 0; i < len / 2; i++) {
+		char j = buffer[i]; buffer[i] = buffer[len-i-1];
+		buffer[len-i-1] = j;
+	}
+
+	return len;
+}
+
+struct mini_buff {
+	char *buffer, *pbuffer;
+	unsigned int buffer_len;
+};
+
+static int buf_putc(int ch, struct mini_buff *b) {
+	if ((unsigned int)((b->pbuffer - b->buffer) + 1) >= b->buffer_len) {
+		return 0;
+	}
+	*(b->pbuffer++) = ch;
+	*(b->pbuffer) = '\0';
+	return 1;
+}
+
+static int buf_puts(char *s, unsigned int len, struct mini_buff *b) {
+	unsigned int i;
+
+	if (b->buffer_len - (b->pbuffer - b->buffer) - 1 < len) {
+		len = b->buffer_len - (b->pbuffer - b->buffer) - 1;
+	}
+
+	/* Copy to buffer */
+	for (i = 0; i < len; i++) {
+		*(b->pbuffer++) = s[i];
+	}
+	*(b->pbuffer) = '\0';
+
+	return len;
+}
+
+int vsnprintf(char *buffer, unsigned int buffer_len, const char *fmt, va_list va) {
+	struct mini_buff b;
+	// buffer for storing format arguments
+	// C standard expects float formats to be as long as 512 chars
+	// so that's the max size of this buffer
+	char bf[24];
+	char ch;
+
+	b.buffer = buffer;
+	b.pbuffer = buffer;
+	b.buffer_len = buffer_len;
+
+	while ((ch=*(fmt++))) {
+		if ((unsigned int)((b.pbuffer - b.buffer) + 1) >= b.buffer_len) {
+			break;
 		}
-
-		const char* format_begun_at = format++;
-
-		if (*format == 'c') {
-			format++;
-			char c = (char) va_arg(parameters, int /* char promotes to int */);
-			if (!maxrem) {
-				// TODO: Set errno to EOVERFLOW.
-				return -1;
-			}
-			if (!print(&c, sizeof(c))) {
-				return -1;
-			}
-			written++;
-		} 
-		else if (*format == 's') {
-			format++;
-			const char* str = va_arg(parameters, const char*);
-			size_t len = strlen(str);
-			if (maxrem < len) {
-				// TODO: Set errno to EOVERFLOW.
-				return -1;
-			}
-			if (!print(str, len)) {
-				return -1;
-			}
-			written += len;
-		} 
+		if (ch!='%') {
+			buf_putc(ch, &b);
+		}
 		else {
-			format = format_begun_at;
-			size_t len = strlen(format);
-			if (maxrem < len) {
-				// TODO: Set errno to EOVERFLOW.
-				return -1;
+			char zero_pad = 0;
+			char *ptr;
+			unsigned int len;
+
+			ch=*(fmt++);
+
+			/* Zero padding requested */
+			if (ch=='0') {
+				ch=*(fmt++);
+				if (ch == '\0') {
+					goto end;
+				}
+				if (ch >= '0' && ch <= '9') {
+					zero_pad = ch - '0';
+				}
+				ch=*(fmt++);
 			}
-			if (!print(format, len)) {
-				return -1;
+
+			switch (ch) {
+				case 0:
+					goto end;
+
+				case 'u':
+				case 'd':
+					len = itoa_advanced(va_arg(va, unsigned int), 10, 0, (ch=='u'), bf, zero_pad);
+					buf_puts(bf, len, &b);
+					break;
+
+				case 'x':
+				case 'X':
+					len = itoa_advanced(va_arg(va, unsigned int), 16, (ch=='X'), 1, bf, zero_pad);
+					buf_puts(bf, len, &b);
+					break;
+
+				case 'c' :
+					buf_putc((char)(va_arg(va, int)), &b);
+					break;
+
+				case 's' :
+					ptr = va_arg(va, char*);
+					buf_puts(ptr, strlen(ptr), &b);
+					break;
+
+				case 'f' :
+					//no-op because we can't have a declaration directly after a label
+					do {} while (0);
+					//double because float gets promoted to double
+					double float_val = va_arg(va, double);
+					ftoa_fixed(bf, float_val);
+					buf_puts(bf, strlen(bf), &b);
+					break;
+
+				default:
+					buf_putc(ch, &b);
+					break;
 			}
-			written += len;
-			format += len;
 		}
 	}
-
-	va_end(parameters);
-	return written;
+end:
+	return b.pbuffer - b.buffer;
 }
+
+int snprintf(char* buffer, unsigned int buffer_len, const char *fmt, ...) {
+	int ret;
+	va_list va;
+	va_start(va, fmt);
+	ret = vsnprintf(buffer, buffer_len, fmt, va);
+	va_end(va);
+
+	return ret;
+}
+
+int printf(const char* fmt, ...) {
+	int ret;
+	va_list va;
+	char buf[512];
+	va_start(va, fmt);
+
+	ret = vsnprintf((char*)buf, sizeof(buf), fmt, va);
+	//TODO(PT): the buffered string should be sent to an stdout handle
+	vga_screen_puts(buf);
+
+	va_end(va);
+	return ret;
+}
+
