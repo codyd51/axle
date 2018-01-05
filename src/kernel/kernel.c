14e113076af7c6050001c87f034349a4e6f73755
Phillip Tennen <pt@datatheorem.com>
2018-01-05 01:06:45 -0500

    add PMM from master/

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,6 +1,18 @@
-#include "axle.h"
+//freestanding headers
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+//kernel stdlib headers
+#include <std/printf.h>
+#include <std/string.h>
+
+//kernel headers
 #include <kernel/drivers/text_mode/text_mode.h>
+#include <kernel/multiboot.h>
+#include <kernel/boot.h>
 #include <kernel/assert.h>
+#include <kernel/boot_info.h>
 
 #define SPIN while (1) {sys_yield(RUNNABLE);}
 #define SPIN_NOMULTI do {} while (1);
@@ -20,4 +32,7 @@ void kernel_main(struct multiboot_info* mboot_ptr, uint32_t initial_stack) {
 
 	boot_info_read(mboot_ptr);
 	boot_info_dump();
+
+	pmm_init();
+	pmm_dump();
 }
