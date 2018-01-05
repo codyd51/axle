c38933f94d7ff38367b50f99817b0ba80203cb40
Phillip Tennen <pt@datatheorem.com>
2018-01-04 21:09:20 -0500

    add setup script

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir -p isodir/boot/grub/
+# TODO get grub-mkrescue
+brew install redox-os/homebrew-gcc_cross_compilers/x86_64-elf-gcc
+brew install redox-os/homebrew-gcc_cross_compilers/x86_64-elf-gdb
