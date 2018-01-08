bbede5ca8ddc678fee318e9a2070d3a85487faa5
Phillip Tennen <pt@datatheorem.com>
2018-01-08 10:39:55 -0500

    change visibility of GDT structures

--- src/kernel/gdt/gdt.c
+++ src/kernel/gdt/gdt.c
@@ -1,6 +1,22 @@
 #include "gdt.h"
 #include <std/memory.h>
 
+
+//Intel-defined GDT structure formats
+//this structure has a carefully defined format which we must preserve
+//see here for format: http://wiki.osdev.org/Global_Descriptor_Table
+struct gdt_entry {
+    uint32_t low_word;
+    uint32_t high_word;
+} __attribute__((packed));
+typedef struct gdt_entry gdt_entry_t;
+
+struct gdt_descriptor {
+    uint16_t table_size;
+    uint32_t table_base;
+} __attribute__((packed));
+typedef struct gdt_descriptor gdt_descriptor_t;
+
 //defined in gdt.s
 extern void gdt_activate(uint32_t* gdt_pointer);
 
