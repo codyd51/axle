8aedbd23e58a6205087f4b49191b28981cd6e664
Phillip Tennen <pt@datatheorem.com>
2018-01-09 18:26:16 -0500

    move kernel/util/interrupts to kernel/interrupts

---
+++ src/kernel/interrupts/isr.h
@@ -0,0 +1,90 @@
+#ifndef ISR_H
+#define ISR_H
+
+#include <std/common.h>
+
+//extern directives allow us to access the addresses of our ASM ISR handlers
+extern void isr0();
+extern void isr1();
+extern void isr2();
+extern void isr3();
+extern void isr4();
+extern void isr5();
+extern void isr6();
+extern void isr7();
+extern void isr8();
+extern void isr9();
+extern void isr10();
+extern void isr11();
+extern void isr12();
+extern void isr13();
+extern void isr14();
+extern void isr15();
+extern void isr16();
+extern void isr17();
+extern void isr18();
+extern void isr19();
+extern void isr20();
+extern void isr21();
+extern void isr22();
+extern void isr23();
+extern void isr24();
+extern void isr25();
+extern void isr26();
+extern void isr27();
+extern void isr28();
+extern void isr29();
+extern void isr30();
+extern void isr31();
+
+extern void irq0();
+extern void irq1();
+extern void irq2();
+extern void irq3();
+extern void irq4();
+extern void irq5();
+extern void irq6();
+extern void irq7();
+extern void irq8();
+extern void irq9();
+extern void irq10();
+extern void irq11();
+extern void irq12();
+extern void irq13();
+extern void irq14();
+extern void irq15();
+extern void irq16();
+extern void irq17();
+
+extern void isr128();
+
+#define IRQ0  32
+#define IRQ1  33
+#define IRQ2  34
+#define IRQ3  35
+#define IRQ4  36
+#define IRQ5  37
+#define IRQ6  38
+#define IRQ7  39
+#define IRQ8  40
+#define IRQ9  41
+#define IRQ10 42
+#define IRQ11 43
+#define IRQ12 44
+#define IRQ13 45
+#define IRQ14 46
+#define IRQ15 47
+
+//enables registration of callbacks for interrupts or IRQs
+//for IRQs, to ease confusion, use #defines above
+//as first parameter
+typedef void (*isr_t)(registers_t);
+void register_interrupt_handler(uint8_t n, isr_t handler);
+void isr_install_default();
+void pic_acknowledge(unsigned int interrupt);
+
+//debug function
+//dumps memory around 'ptr' to kernel log
+void dump_stack(uint32_t* ptr);
+
+#endif
