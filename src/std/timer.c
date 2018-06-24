602e631fb1da6d78c707ea94306c9960a5e72e3b
Phillip Tennen <pt@datatheorem.com>
2018-06-24 20:24:54 +0200

    add timer_deliver_immediately, change timer_callback_t->callback to timer_callback_t->func

--- src/std/timer.c
+++ src/std/timer.c
@@ -6,16 +6,16 @@
 #include <kernel/syscall/sysfuncs.h>
 
 static int callback_num = 0;
-static timer_callback callback_table[MAX_CALLBACKS];
+static timer_callback_t callback_table[MAX_CALLBACKS];
 
 static void clear_table() {
-	memset(&callback_table, 0, sizeof(timer_callback) * callback_num);
+	memset(&callback_table, 0, sizeof(timer_callback_t) * callback_num);
 	callback_num = 0;
 }
 
 static int next_open_callback_index() {
 	for (int i = 0; i < callback_num; i++) {
-		if (!callback_table[i].callback) {
+		if (!callback_table[i].func) {
 			//this index doesn't have valid data, fit for reuse
 			return i;
 		}
@@ -24,11 +24,11 @@ static int next_open_callback_index() {
 	return callback_num;
 }
 
-timer_callback add_callback(void* callback, int interval, bool repeats, void* context) {
+timer_callback_t* add_callback(void* func, int interval, bool repeats, void* context) {
 	int next_open_index = next_open_callback_index();
 	//only add callback if we have room
 	if (callback_num + 1 < MAX_CALLBACKS || next_open_index < callback_num) {
-		callback_table[callback_num].callback = callback;
+		callback_table[callback_num].func = func;
 		callback_table[callback_num].interval = interval;
 		callback_table[callback_num].time_left = interval;
 		callback_table[callback_num].repeats = repeats;
@@ -36,25 +36,31 @@ timer_callback add_callback(void* callback, int interval, bool repeats, void* co
 
 		callback_num++;
 
-		return callback_table[callback_num];
+		return &(callback_table[callback_num]);
 	}
 
 	//TODO expand table instead of clearing it
 	clear_table();
 	//try adding the callback again now that we know the table has room
-	return add_callback(callback, interval, repeats, context);
+	return add_callback(func, interval, repeats, context);
 }
 
-void remove_callback(timer_callback callback) {
+void remove_callback(timer_callback_t* callback) {
 	//find this callback in callback table
+	bool found = false;
 	for (int i = 0; i < callback_num; i++) {
-		if (callback_table[callback_num].callback == callback.callback) {
-			memset(&callback_table[i], 0, sizeof(timer_callback));
+		if (callback_table[callback_num].func == callback->func) {
+			found = true;
+			break;
 		}
 	}
+	if (!found) {
+		panic("tried to delete nonexistant timer callback. investigate!");
+	}
+	memset(callback, 0, sizeof(timer_callback_t));
 }
 
-void handle_tick(registers_t* register_state) {
+void _timer_handle_pit_tick(registers_t* register_state) {
 	//look through every callback and see if we should fire
 	for (int i = 0; i < callback_num; i++) {
 		//decrement time left
@@ -65,18 +71,23 @@ void handle_tick(registers_t* register_state) {
 			//reset for next firing
 			callback_table[i].time_left = callback_table[i].interval;
 
-			void(*callback_func)(registers_t*, void*) = (void(*)(registers_t*, void*))callback_table[i].callback;
+			void(*callback_func)(registers_t*, void*) = (void(*)(registers_t*, void*))callback_table[i].func;
 			callback_func(register_state, callback_table[i].context);
 
 			//if we only fire once, trash this callback
 			if (!callback_table[i].repeats) {
-				remove_callback(callback_table[i]);
+				remove_callback(&(callback_table[i]));
 			}
 		}
 	}
 }
 
+void timer_deliver_immediately(timer_callback_t* callback) {
+	callback->time_left = 0;
+}
+
 void sleep(uint32_t ms) {
+	Deprecated();
 	uint32_t end = time() + ms;
     extern task_t* current_task;
     current_task->wake_timestamp = end;
--- src/std/timer.h
+++ src/std/timer.h
@@ -10,16 +10,21 @@ __BEGIN_DECLS
 #define MAX_CALLBACKS 100
 
 typedef struct {
-	void* callback;
+	void* func;
 	uint32_t interval;
 	uint32_t time_left;
 	bool repeats;
 	void* context;
-} timer_callback;
+} timer_callback_t;
+
+STDAPI timer_callback_t* timer_add_callback(void* callback, int interval, bool repeats, void* context);
+STDAPI void timer_remove_callback(timer_callback_t* callback);
+STDAPI void timer_deliver_immediately(timer_callback_t* callback);
 
 STDAPI void sleep(uint32_t ms);
-STDAPI timer_callback add_callback(void* callback, int interval, bool repeats, void* context);
-STDAPI void remove_callback(timer_callback callback);
+
+//friend function for pit.c
+void _timer_handle_pit_tick();
 
 __END_DECLS
 
