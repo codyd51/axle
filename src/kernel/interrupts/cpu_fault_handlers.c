31555ef9d577aa94423c9b9df30bd9023c3e2d2d
Phillip Tennen <pt@datatheorem.com>
2018-06-27 19:59:09 +0200

    CPU fault handlers take a register_state_t* instead of register_state

--- src/kernel/interrupts/cpu_fault_handlers.c
+++ src/kernel/interrupts/cpu_fault_handlers.c
@@ -5,74 +5,74 @@
 
 #include <kernel/assert.h>
 
-static void common_halt(register_state_t regs, bool recoverable) {
+static void common_halt(register_state_t* regs, bool recoverable) {
     printf("Halt.\n");
     panic("Common halt not implemented");
 }
 
-void interrupt_handle_divide_by_zero(register_state_t regs) {
+void interrupt_handle_divide_by_zero(register_state_t* regs) {
 	printf_err("Attempted division by zero");
 	common_halt(regs, false);
 }
 
-void interrupt_handle_bound_range_exceeded(register_state_t regs) {
+void interrupt_handle_bound_range_exceeded(register_state_t* regs) {
 	printf_err("Bound range exception");
 	common_halt(regs, false);
 }
 
-void interrupt_handle_invalid_opcode(register_state_t regs) {
+void interrupt_handle_invalid_opcode(register_state_t* regs) {
 	printf_err("Invalid opcode encountered");
 	common_halt(regs, false);
 }
 
-void interrupt_handle_device_not_available(register_state_t regs) {
+void interrupt_handle_device_not_available(register_state_t* regs) {
 	printf_err("Device not available");
 	common_halt(regs, false);
 }
 
-void interrupt_handle_double_fault(register_state_t regs) {
+void interrupt_handle_double_fault(register_state_t* regs) {
 	printf_err("=======================");
 	printf_err("Caught double fault!");
 	printf_err("=======================");
 	common_halt(regs, false);
 }
 
-void interrupt_handle_invalid_tss(register_state_t regs) {
+void interrupt_handle_invalid_tss(register_state_t* regs) {
 	printf_err("Invalid TSS section!");
 	common_halt(regs, false);
 }
 
-void interrupt_handle_segment_not_present(register_state_t regs) {
+void interrupt_handle_segment_not_present(register_state_t* regs) {
 	printf_err("Segment not present");
 	common_halt(regs, false);
 }
 
-void interrupt_handle_stack_segment_fault(register_state_t regs) {
+void interrupt_handle_stack_segment_fault(register_state_t* regs) {
 	printf_err("Stack segment fault");
 	common_halt(regs, false);
 }
 
-void interrupt_handle_general_protection_fault(register_state_t regs) {
+void interrupt_handle_general_protection_fault(register_state_t* regs) {
 	printf_err("General protection fault");
 	common_halt(regs, false);
 }
 
-void interrupt_handle_floating_point_exception(register_state_t regs) {
+void interrupt_handle_floating_point_exception(register_state_t* regs) {
 	printf_err("Floating point exception");
 	common_halt(regs, false);
 }
 
-void interrupt_handle_alignment_check(register_state_t regs) {
+void interrupt_handle_alignment_check(register_state_t* regs) {
 	printf_err("Alignment check");
 	common_halt(regs, false);
 }
 
-void interrupt_handle_machine_check(register_state_t regs) {
+void interrupt_handle_machine_check(register_state_t* regs) {
 	printf_err("Machine check");
 	common_halt(regs, false);
 }
 
-void interrupt_handle_virtualization_exception(register_state_t regs) {
+void interrupt_handle_virtualization_exception(register_state_t* regs) {
 	printf_err("Virtualization exception");
 	common_halt(regs, false);
 }
