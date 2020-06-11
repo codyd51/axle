300d438c8d522772b56b04b450c6b9bf2c42b424
Phillip Tennen <pt@datatheorem.com>
2020-06-12 00:14:43 +0200

    panic() reports the error message

--- src/kernel/assert.c
+++ src/kernel/assert.c
@@ -1,7 +1,7 @@
 #include "assert.h"
 
 void _panic(const char* msg, const char* file, int line) {
-    printf("Kernel assertion in %s line %d", file, line);
+    printf("Kernel assertion in %s line %d: %s", file, line, msg);
     //enter infinite loop
     asm("cli");
     do {} while (1);
