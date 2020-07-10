d18397eb653ba4e9d8c44839cb4c13701af1f9fa
Phillip Tennen <pt@datatheorem.com>
2020-07-10 14:09:52 +0200

    Boot to 1920x1080 via grub

--- src/boot/boot.s
+++ src/boot/boot.s
@@ -1,8 +1,9 @@
+; Multiboot v0.6.96 spec: https://www.gnu.org/software/grub/manual/multiboot/multiboot.html#Header-graphics-fields 
 MBOOT_PAGE_ALIGN	equ 1<<0	; load kernel and modules on page boundary
 MBOOT_MEM_MAP		equ 1<<1	; provide kernel. with memory info
 MBOOT_VIDEO_MODE	equ 1<<2	; have GRUB set video mode
 MBOOT_HEADER_MAGIC	equ 0x1BADB002	; multiboot magic value
-MBOOT_HEADER_FLAGS	equ MBOOT_PAGE_ALIGN | MBOOT_MEM_MAP ;| MBOOT_VIDEO_MODE
+MBOOT_HEADER_FLAGS	equ MBOOT_PAGE_ALIGN | MBOOT_MEM_MAP | MBOOT_VIDEO_MODE
 MBOOT_CHECKSUM		equ -(MBOOT_HEADER_MAGIC + MBOOT_HEADER_FLAGS)
 
 [bits 32]
@@ -11,6 +12,11 @@ MBOOT_CHECKSUM		equ -(MBOOT_HEADER_MAGIC + MBOOT_HEADER_FLAGS)
 dd MBOOT_HEADER_MAGIC	; header value for GRUB
 dd MBOOT_HEADER_FLAGS	; grub settings
 dd MBOOT_CHECKSUM		; ensure above values are correct
+dd 0, 0, 0, 0, 0	; Unused flags for specifying load positions
+dd 0	; Graphics mode type (0 = linear graphics, 1 = EGA-standard text mode)
+dd 1920	; Graphics px width
+dd 1080	; Graphics px height
+dd 24	; Graphics bits per pixel
 
 [section .text]
 [global _start]
