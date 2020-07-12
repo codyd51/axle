e7cf708ad1bd4fbaf5433e6d790699b0291240ec
Phillip Tennen <pt@datatheorem.com>
2020-07-12 16:51:11 +0200

    Refactor boot screens into their own file

---
+++ src/gfx/lib/boot_screens.h
@@ -0,0 +1,13 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include "gfx.h"
+
+// Synchronously display the primary boot screen.
+// Includes boot logo and "rainbow" loading animation
+void display_boot_screen(void);
+
+// Synchronously display the "Daisy" novelty boot screen.
+void display_daisy_screen(void);
+
+#endif
