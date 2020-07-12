e6fbfdf6873a91543fb3647ecbf15726175a9167
Phillip Tennen <pt@datatheorem.com>
2020-07-12 22:23:06 +0200

    Update build scripts to point to new newlib port

--- build-newlib.sh
+++ build-newlib.sh
@@ -5,9 +5,9 @@ ln ./i686-toolchain/bin/i686-elf-gcc i686-axle-gcc
 ln ./i686-toolchain/bin/i686-elf-gcc i686-axle-cc
 ln ./i686-toolchain/bin/i686-elf-ranlib i686-axle-ranlib
 export PATH=./i686-toolchain/bin/:$PATH
-export PATH=/Users/philliptennen/Documents/develop/axle/newlib/newlib_bin/bin/:$PATH
-export PATH=~/Documents/develop/axle/i686-toolchain/bin/:$PATH
-cd "./newlib/build-newlib"
+export PATH=/Users/philliptennen/Documents/develop/axle/ports/newlib/bin/bin/:$PATH
+cd "./ports/newlib/build-newlib"
 pwd
+# ../newlib-2.5.0.20171222/configure --prefix=/usr --target=i686-axle
 make all
-make DESTDIR=~/Documents/develop/axle/axle-sysroot-2020 install
+make DESTDIR=~/Documents/develop/axle/axle-sysroot install
