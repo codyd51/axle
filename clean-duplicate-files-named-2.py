6d6346e5f3f8a575047bf55cda312a15cdfd6ce8
Phillip Tennen <pt@datatheorem.com>
2020-07-16 14:16:34 +0200

    Add build-script to delete files named “original_name 2.ext”

---
+++ clean-duplicate-files-named-2.py
@@ -0,0 +1,20 @@
+"""I don't know why, but sometimes when working with VS Code
+duplicates of many working files pop up with " 2" in the filename.
+It pollutes my working index and causes problems with the build system.
+This script finds and deletes them.
+"""
+import os
+import sys
+import shutil
+import pathlib
+
+
+def main():
+    root_dir = pathlib.Path(__file__).parent
+    for f in root_dir.rglob("*"):
+        if " 2" in f.name:
+            os.remove(f.as_posix())
+
+
+if __name__ == '__main__':
+    main()
