7c7636a93bbc2d92048af03f45d2000668619811
Phillip Tennen <pt@datatheorem.com>
2020-07-18 19:00:55 +0200

    PS/2 keyboard driver uses sysroot’s amc header

--- src/user/extern/kb_driver/kb_driver.c
+++ src/user/extern/kb_driver/kb_driver.c
@@ -4,39 +4,13 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#include <kernel/amc.h>
+
 #include "kb_driver.h"
 #include "kb_us.h"
 #include "kb_colemak.h"
 
 static keymap_t* _active_kb_layout = 0;
-
-typedef enum amc_message_type {
-    KEYSTROKE = 0,
-    STDOUT = 1,
-} amc_message_type_t;
-
-typedef struct amc_message {
-    const char* source;
-    const char* dest; // May be null if the message is globally broadcast
-    amc_message_type_t type;
-    char data[64];
-    int len;
-} amc_message_t;
-
-// Register the running process as the provided service name
-void amc_register_service(const char* name);
-
-// Construct an amc message
-amc_message_t* amc_message_construct(amc_message_type_t type, const char* data, int len);
-
-// Asynchronously send the message to the provided destination service
-void amc_message_send(const char* destination_service, amc_message_t* msg);
-
-// Asynchronously send the message to any service awaiting a message from this service
-void amc_message_broadcast(amc_message_t* msg);
-
-// Block until a message has been received from the source service
-void amc_message_await(const char* source_service, amc_message_t* out);
 
 static void _set_active_kb_layout(keymap_t* new_layout) {
 	_active_kb_layout = new_layout;
@@ -97,8 +71,8 @@ static char _translate_scancode_to_character(uint8_t scancode) {
 }
 
 int main(int argc, char** argv) {
-	_set_active_kb_layout(&kb_colemak);
 	amc_register_service("com.axle.kb_driver");
+	_set_active_kb_layout(&kb_colemak);
 
 	while (true) {
 		// The message from the low-level keyboard driver will contain the bare scancode
@@ -108,7 +82,7 @@ int main(int argc, char** argv) {
 		char translated_key = _translate_scancode_to_character(scancode);
 		// Ignore the keypress if it wasn't a newly pressed regular character
 		if (translated_key) {
-			amc_message_t* keypress_msg = amc_message_construct(KEYSTROKE, &translated_key, 1);
+			amc_message_t* keypress_msg = amc_message_construct(&translated_key, 1);
 			//amc_message_broadcast(keypress_msg);
 			amc_message_send("com.axle.awm", keypress_msg);
 		}
