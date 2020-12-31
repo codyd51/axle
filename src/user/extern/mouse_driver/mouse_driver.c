826f33949e5daa3e272f8541970e2091bd562218
Phillip Tennen <pt@datatheorem.com>
2020-12-31 17:40:31 +0000

    Check in PS2 mouse driver

--- src/user/extern/mouse_driver/mouse_driver.c
+++ src/user/extern/mouse_driver/mouse_driver.c
@@ -8,147 +8,53 @@
 
 #include "mouse_driver.h"
 
-typedef unsigned char byte;
-typedef signed char sbyte;
-typedef unsigned int dword;
-
-static int running_x = -1;
-static int running_y = -1;
-volatile uint8_t mouse_state;
-
-uint8_t inb(uint16_t port) {
-	uint8_t _v;
-	__asm__ __volatile__ ("inb %w1,%0":"=a" (_v):"Nd" (port));
-	return _v;
-}
-
-void outb(uint16_t port, uint8_t val) {
-	 asm volatile("outb %0, %1" : : "a"(val), "Nd"(port) );
-}
-
-static void mouse_wait(byte a_type) {
-	dword timeout = 100000;
-	if (a_type == 0) {
-		while (timeout--) {
-			if ((inb(0x64) & 1) == 1) {
-				return;
-			}
-		}
-		return;
-	}
-	else {
-		while (timeout--) {
-			if ((inb(0x64) & 2) == 0) {
-				return;
-			}
-		}
-		return;
-	}
-}
-
-static void mouse_write(byte a) {
-	//wait to be able to send a command
-	mouse_wait(1);
-	//tell mouse we're sending a command
-	outb(0x64, 0xD4);
-	//wait for final part
-	mouse_wait(1);
-	//write
-	outb(0x60, a);
-}
-
-static byte mouse_read() {
-	//get response from mouse
-	mouse_wait(0);
-	return inb(0x60);
-}
-
-static void _process_mouse_event(uint8_t data) {
-	static sbyte mouse_byte[3];
-	static byte mouse_cycle = 0;
-
-	switch (mouse_cycle) {
-		case 0:
-			mouse_byte[0] = inb(0x60);
-			mouse_cycle++;
-
-			//this byte contains information about mouse state (button events)
-			bool middle = mouse_byte[0] & 0x4;
-			if (middle) mouse_state |= 0x4;
-			else mouse_state &= ~0x4;
-
-			bool right = mouse_byte[0] & 0x2;
-			if (right) mouse_state |= 0x2;
-			else mouse_state &= ~0x2;
-
-			bool left = mouse_byte[0] & 0x1;
-			if (left) mouse_state |= 0x1;
-			else mouse_state &= ~0x1;
-
-			break;
-		case 1:
-			mouse_byte[1] = inb(0x60);
-			mouse_cycle++;
-
-			break;
-		case 2:
-			mouse_byte[2] = inb(0x60);
-
-			uint8_t state = mouse_byte[0];
-			uint8_t byte = mouse_byte[1];
-			int8_t rel_x = byte - ((state << 4) & 0x100);
-			byte = mouse_byte[2];
-			int8_t rel_y = byte - ((state << 3) & 0x100);
-
-			//printf("Core mouse event %d %d\n", rel_x, rel_y);
-			int8_t mouse_databuf[4];
-			mouse_databuf[0] = state;
-			mouse_databuf[1] = rel_x;
-			mouse_databuf[2] = rel_y;
-			//mouse_databuf[1] = mouse_byte[1];
-			//mouse_databuf[2] = mouse_byte[2];
-			amc_message_t* amc_msg = amc_message_construct(&mouse_databuf, sizeof(mouse_databuf));
-			amc_message_send("com.axle.awm", amc_msg);
-			printf("mouse driver mouse event: %d %d %d\n", state, rel_x, rel_y);
-
-			mouse_cycle = 0;
-
-		default:
-			mouse_cycle = 0;
-			break;
-	}
-}
+typedef struct ps2_mouse_state {
+	uint8_t idx;
+	uint8_t buffer[3];
+} ps2_mouse_state_t;
 
 int main(int argc, char** argv) {
 	amc_register_service("com.axle.mouse_driver");
 
+	ps2_mouse_state_t state = {0, 0};
+
 	while (true) {
-		// The message from the low-level keyboard driver will contain the bare scancode
+		// The message from the low-level mouse driver will contain a data packet
 		amc_message_t msg = {0};
 		amc_message_await("com.axle.core", &msg);
-		uint8_t state = msg.data[0];
-		uint8_t rel_x = msg.data[1];
-		uint8_t rel_y = msg.data[2];
-		//mouse_databuf[1] = mouse_byte[1];
-		//mouse_databuf[2] = mouse_byte[2];
-		int8_t mouse_databuf[4];
-		mouse_databuf[0] = state;
-		mouse_databuf[1] = rel_x;
-		mouse_databuf[2] = rel_y;
-		//mouse_databuf[1] = mouse_byte[1];
-		//mouse_databuf[2] = mouse_byte[2];
-		amc_message_t* amc_msg = amc_message_construct(&mouse_databuf, sizeof(mouse_databuf));
-		amc_message_send("com.axle.awm", amc_msg);
-		printf("mouse driver mouse event: %d %d %d\n", state, rel_x, rel_y);
-		//_process_mouse_event(&msg);
-		//uint8_t m0 = msg.data[0];
-		//uint8_t m1 = msg.data[1];
-		//uint8_t m2 = msg.data[2];
-		//char chbuf[256];
-		//snprintf(&chbuf, sizeof(chbuf), "Received mouse event: %d %d\n", m1, m2);
-		//amc_message_t* keypress_msg = amc_message_construct(&chbuf, sizeof(chbuf));
-		//amc_message_send("com.axle.awm", keypress_msg);
-		//printf("Received mouse event: %d %d\n", m1, m1);
+		uint8_t data_packet = msg.data[0];
+		state.buffer[state.idx] = data_packet;
+		if (state.idx == 0) {
+			state.idx += 1;
+
+			// This byte contains info about button events
+			/*
+			bool middle_button_pressed = data_packet & (1 << 2);
+			bool right_button_pressed = data_packet & (1 << 1);
+			bool left_button_pressed = data_packet & (1 << 0);
+			*/
+		}
+		else if (state.idx == 1) {
+			state.idx += 1;
+		}
+		else {
+			uint8_t status_byte = state.buffer[0];
+			// Invert the X and Y values if the status packet indicates they're negative
+			int8_t rel_x = state.buffer[1] - ((status_byte << 4) & 0x100);
+			// Always flip the Y axis as it arrives
+			int8_t rel_y = -(state.buffer[2]) - ((status_byte << 5) & 0x100);
+
+			state.idx = 0;
+			memset(&state.buffer, 0, sizeof(state.buffer));
+
+			int8_t mouse_databuf[3] = {0};
+			mouse_databuf[0] = status_byte;
+			mouse_databuf[1] = rel_x;
+			mouse_databuf[2] = rel_y;
+			amc_message_t* amc_msg = amc_message_construct(&mouse_databuf, sizeof(mouse_databuf));
+			amc_message_send("com.axle.awm", amc_msg);
+		}
+
 	}
 	
 	return 0;
