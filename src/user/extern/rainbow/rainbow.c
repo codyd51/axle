149fa3887253b9685749129beccdba85a7ff1bc8
Phillip Tennen <pt@datatheorem.com>
2021-01-12 02:56:18 +0100

    API updates to rainbow app

--- src/user/extern/rainbow/rainbow.c
+++ src/user/extern/rainbow/rainbow.c
@@ -12,6 +12,8 @@
 #include <agx/lib/ca_layer.h>
 #include <agx/lib/putpixel.h>
 
+#include <awm/awm.h>
+
 #include "gfx.h"
 
 // Many graphics lib functions call gfx_screen() 
@@ -39,51 +41,31 @@ float pifdist(int x1, int y1, int x2, int y2) {
 
 void _radial_gradiant(ca_layer* layer, Size gradient_size, Color c1, Color c2, int x1, int y1, float r) {
 	for (uint32_t y = 0; y < gradient_size.height; y++) {
+		//printf("draw row %d\n", y);
 		for (uint32_t x = 0; x < gradient_size.width; x++) {
 			putpixel(layer, x, y, transcolor(c1, c2, pifdist(x1, y1, x, y) / r));
 		}
 	}
 }
 
-static void _handle_awm_message(amc_message_t awm_msg, ca_layer* window_layer) {
-	Size window_size = size_make(800, 600);
-	if (!strcmp(awm_msg.data, "redraw")) {
-		/*
-		int r = rand() % 255;
-		int g = rand() % 255;
-		int b = rand() % 255;
-		Color c = color_make(r, g, b);
-		draw_rect(window_layer, rect_make(point_zero(), size_make(800, 600)), c, THICKNESS_FILLED);
-		*/
-		Color c1 = color_make(rand() % 255, rand() % 255, rand() % 255);
-		Color c2 = color_make(rand() % 255, rand() % 255, rand() % 255);
-		//_radial_gradiant(window_layer, window_size, c1, c2, window_size.width/2, window_size.height / 2, (float)window_size.height/4);
-		_radial_gradiant(window_layer, window_size, c1, c2, window_size.width/2, 200, (float)window_size.height/4);
-
-		const char* cmd2 = "update_framebuf";
-		amc_message_t* draw_framebuf_msg = amc_message_construct(cmd2, strlen(cmd2));
-		amc_message_send("com.axle.awm", draw_framebuf_msg);
-	}
-}
-
-int main(int argc, char** argv) {
-	amc_register_service("com.user.rainbow");
-	debug_awm();
-	printf("Rainbow (PID [%d]) running!\n", getpid());
-	
-	const char* cmd = "get_framebuf";
-	amc_message_t* get_framebuf_msg = amc_message_construct(cmd, strlen(cmd));
-	amc_message_send("com.axle.awm", get_framebuf_msg);
-	amc_message_t receive_framebuf = {0};
+static ca_layer* window_layer_get(uint32_t width, uint32_t height) {
+	// Ask awm to make a window for us
+	amc_msg_u32_3__send("com.axle.awm", AWM_REQUEST_WINDOW_FRAMEBUFFER, width, height);
+	// And get back info about the window it made
+	amc_command_ptr_message_t receive_framebuf = {0};
 	amc_message_await("com.axle.awm", &receive_framebuf);
-	uint32_t framebuffer_addr = (uint32_t)strtol(receive_framebuf.data, NULL, 0);
-	//printf("Received framebuffer from awm: %s 0x%08x\n", receive_framebuf.data, framebuffer_addr);
-	framebuffer_addr = 0xa0000000;
+	// TODO(PT): Need a struct type selector
+	if (amc_command_ptr_msg__get_command(&receive_framebuf) != AWM_CREATED_WINDOW_FRAMEBUFFER) {
+		printf("Invalid state. Expected framebuffer command\n");
+	}
+
+	printf("Received framebuffer from awm: %d 0x%08x\n", amc_command_ptr_msg__get_command(&receive_framebuf), amc_command_ptr_msg__get_ptr(&receive_framebuf));
+	uint32_t framebuffer_addr = receive_framebuf.body.cmd_ptr.ptr_val;
 	uint8_t* buf = (uint8_t*)framebuffer_addr;
 
 	// TODO(PT): Use an awm command to get screen info
 	_screen.resolution = size_make(1920, 1080);
-	_screen.physbase = (uint32_t*)framebuffer_addr;
+	_screen.physbase = (uint32_t*)0;
 	_screen.bits_per_pixel = 32;
 	_screen.bytes_per_pixel = 4;
 
@@ -94,21 +76,27 @@ int main(int argc, char** argv) {
 	dummy_layer->alpha = 1.0;
     _screen.vmem = dummy_layer;
 
-	draw_rect(_screen.vmem, rect_make(point_zero(), size_make(800, 600)), color_green(), THICKNESS_FILLED);
+	return dummy_layer;
+}
 
-	int i = 0; 
+int main(int argc, char** argv) {
+	amc_register_service("com.user.rainbow");
+
+	printf("Rainbow (PID [%d]) running!\n", getpid());
+	Size window_size = size_make(300, 300);
+	ca_layer* window_layer = window_layer_get(window_size.width, window_size.height);
+	draw_rect(window_layer, rect_make(point_zero(), window_size), color_red(), THICKNESS_FILLED);
+	amc_command_msg__send("com.axle.awm", AWM_WINDOW_REDRAW_READY);
+
+	int i = 0;
 	while (true) {
-		i++;
-		amc_message_t msg = {0};
-		amc_message_await("com.axle.awm", &msg);
-
-		if (!strcmp(msg.source, "com.axle.awm")) {
-			_handle_awm_message(msg, dummy_layer);
-		}
-		else {
-			printf("Unrecognized message: %s\n", msg.source);
-			continue;
-		}
+		// TODO(PT): For some reason interrupts are disabled here - fix it
+		asm("sti");
+		Color c1 = color_make(rand() % 255, rand() % 255, rand() % 255);
+		Color c2 = color_make(rand() % 255, rand() % 255, rand() % 255);
+		//_radial_gradiant(window_layer, window_size, c1, c2, window_size.width/2, window_size.height / 2, (float)window_size.height/4);
+		_radial_gradiant(window_layer, window_size, c1, c2, window_size.width/2, 200, (float)window_size.height/4);
+		amc_command_msg__send("com.axle.awm", AWM_WINDOW_REDRAW_READY);
 	}
 
 	return 0;
