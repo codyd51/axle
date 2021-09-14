5d86a41d4c4191bf45891addb8d462990b9031f1
Phillip Tennen <pt@datatheorem.io>
2021-09-14 19:49:23 +0100

    Add script to generate a QEMU emulated hard drive

---
+++ create_hard_drive_image.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python3
+from build_utils import run_and_check
+
+
+def main() -> None:
+    run_and_check(['qemu-img', 'create', '-f', 'raw', 'axle-hdd.img', '64M'])
+
+
+if __name__ == '__main__':
+    main()
