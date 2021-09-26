2ae862d67404c59349b5db213c9d8a47b87fed57
Phillip Tennen <pt@datatheorem.io>
2021-09-26 19:03:31 +0200

    [Build] Scripts allow forcing rebuilds of specified programs, or everything

--- build_programs.py
+++ build_programs.py
@@ -3,6 +3,7 @@ import os
 import time
 import concurrent.futures
 from build_utils import run_and_check
+from typing import Optional, List
 from pathlib import Path
 
 
@@ -16,7 +17,7 @@ def recompile_program(program_dir: Path) -> None:
     run_and_check(['meson', 'install', '-C', 'build', '--only-changed'], cwd=program_dir)
 
 
-def build_all_programs(only_recently_updated: bool = False) -> None:
+def build_all_programs(only_recently_updated: bool = False, force_rebuild_programs: Optional[List[str]] = None, force_rebuild_all=False) -> None:
     programs_root = Path(__file__).parent / "programs"
     cross_compile_config = programs_root / "cross_axle.ini"
 
@@ -28,14 +29,24 @@ def build_all_programs(only_recently_updated: bool = False) -> None:
         if program_dir.name in SKIP_PROGRAMS:
             continue
 
-        if only_recently_updated and not any([f.is_file() and os.stat(f.as_posix()).st_atime >= time.time() - 180 for f in program_dir.iterdir()]):
-            continue
-
+        if only_recently_updated and not force_rebuild_all:
+            if not any([f.is_file() and os.stat(f.as_posix()).st_atime >= time.time() - 180 for f in program_dir.iterdir()]):
+                # Only exclude not-recently-updated programs if not specified in programs that should be force rebuilt
+                if not force_rebuild_programs:
+                    continue
+                if program_dir.name not in force_rebuild_programs:
+                    continue
+        
         meson_build_file = program_dir / 'meson.build'
         if not meson_build_file.exists():
             raise RuntimeError(f'Program not using Meson: {program_dir}')
-        
+
         build_folder = program_dir / "build"
+
+        if force_rebuild_all or (force_rebuild_programs and program_dir.name in force_rebuild_programs):
+            print(f'Forcing rebuild of {program_dir}...')
+            run_and_check(['rm', '-rf', build_folder.as_posix()])
+        
         if not build_folder.exists():
             print(f'Running one-time Meson configuration in {program_dir}...')
             run_and_check(['meson', 'build', '--cross-file', cross_compile_config.as_posix()], cwd=program_dir)
--- build_quick.py
+++ build_quick.py
@@ -1,14 +1,22 @@
 #!/usr/local/bin/python3
 from pathlib import Path
 
+import argparse
+
 from build_utils import run_and_check
 from build_programs import build_all_programs
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--force_rebuild_programs', nargs='*', action='store')
+    parser.add_argument('--force_rebuild_everything', action='store_true')
+    parser.set_defaults(force_rebuild_everything=False)
+    args = parser.parse_args()
+
     parent_folder = Path(__file__).parent
 
-    build_all_programs(only_recently_updated=True)
+    build_all_programs(only_recently_updated=True, force_rebuild_programs=args.force_rebuild_programs, force_rebuild_all=args.force_rebuild_everything)
 
     run_and_check(['bash', 'build-initrd-and-run.sh'], cwd=parent_folder)
 
