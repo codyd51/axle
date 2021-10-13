558f332e80876cfd679ae608f63011a50d738b09
Phillip Tennen <pt@datatheorem.io>
2021-10-13 17:58:50 +0200

    [VFS] Check in updated headers

--- axle-sysroot/usr/i686-axle/include/awm_messages.h
+++ axle-sysroot/usr/i686-axle/include/awm_messages.h
@@ -21,7 +21,7 @@
 
 #define AWM_MOUSE_MOVED 804
 typedef struct awm_mouse_moved_msg {
-    uint32_t event; // AWM_MOUSE_MOVED
+    uint32_t event; // Expects AWM_MOUSE_MOVED
     uint32_t x_pos;
     uint32_t y_pos;
 } awm_mouse_moved_msg_t;
@@ -31,32 +31,32 @@ typedef struct awm_mouse_moved_msg {
 
 #define AWM_MOUSE_SCROLLED 807
 typedef struct awm_mouse_scrolled_msg {
-    uint32_t event; // AWM_MOUSE_SCROLLED
+    uint32_t event; // Expects AWM_MOUSE_SCROLLED
     int8_t delta_z;
 } awm_mouse_scrolled_msg_t;
 
 #define AWM_WINDOW_RESIZED 808
 typedef struct awm_window_resized_msg {
-    uint32_t event; // AWM_WINDOW_RESIZED
+    uint32_t event; // Expects AWM_WINDOW_RESIZED
     Size new_size;
 } awm_window_resized_msg_t;
 
 #define AWM_MOUSE_LEFT_CLICK 809
 typedef struct awm_mouse_left_click_msg {
-    uint32_t event; // AWM_MOUSE_LEFT_CLICK
+    uint32_t event; // Expects AWM_MOUSE_LEFT_CLICK
     Point click_point;
 } awm_mouse_left_click_msg_t;
 
 #define AWM_MOUSE_DRAGGED 810
 typedef struct awm_mouse_dragged_msg {
-    uint32_t event; // AWM_MOUSE_DRAGGED
+    uint32_t event; // Expects AWM_MOUSE_DRAGGED
     uint32_t x_pos;
     uint32_t y_pos;
 } awm_mouse_dragged_msg_t;
 
 #define AWM_MOUSE_LEFT_CLICK_ENDED 811
 typedef struct awm_mouse_left_click_ended_msg {
-    uint32_t event; // AWM_MOUSE_LEFT_CLICK_ENDED
+    uint32_t event; // Expects AWM_MOUSE_LEFT_CLICK_ENDED
     Point click_end_point;
 } awm_mouse_left_click_ended_msg_t;
 
@@ -66,7 +66,7 @@ typedef struct awm_mouse_left_click_ended_msg {
 // Sent from a client to awm
 #define AWM_UPDATE_WINDOW_TITLE 813
 typedef struct awm_window_title_msg {
-    uint32_t event; // AWM_UPDATE_WINDOW_TITLE
+    uint32_t event; // Expects AWM_UPDATE_WINDOW_TITLE
     uint32_t len;
     char title[64];
 } awm_window_title_msg_t;
--- axle-sysroot/usr/i686-axle/include/file_manager/file_manager_messages.h
+++ axle-sysroot/usr/i686-axle/include/file_manager/file_manager_messages.h
@@ -7,46 +7,6 @@
 #define FILE_MANAGER_MAX_PATH_LENGTH 128
 
 /*
- * Read a file
- */
-
-// Sent from clients to the file manager
-#define FILE_MANAGER_READ_FILE 100
-typedef struct file_manager_read_file_request {
-    uint32_t event; // FILE_MANAGER_READ_FILE 
-    char path[FILE_MANAGER_MAX_PATH_LENGTH];
-} file_manager_read_file_request_t;
-
-// Sent from the file manager to clients
-#define FILE_MANAGER_READ_FILE_RESPONSE 101
-typedef struct file_manager_read_file_response {
-    uint32_t event; // FILE_MANAGER_READ_FILE_RESPONSE
-    uint32_t file_size;
-    uint8_t file_data[];
-} file_manager_read_file_response_t;
-
-/*
- * Read a segment of a file
- */
-
-// Sent from clients to the file manager
-#define FILE_MANAGER_READ_FILE__PARTIAL 102
-typedef struct file_manager_read_file_partial_request {
-    uint32_t event; // FILE_MANAGER_READ_FILE 
-    uint32_t offset;
-    uint32_t length;
-    char path[FILE_MANAGER_MAX_PATH_LENGTH];
-} file_manager_read_file_partial_request_t;
-
-// Sent from the file manager to clients
-#define FILE_MANAGER_READ_FILE__PARTIAL_RESPONSE 102
-typedef struct file_manager_read_file_partial_response {
-    uint32_t event; // FILE_MANAGER_READ_FILE__PARTIAL_RESPONSE
-    uint32_t data_length;
-    uint8_t file_data[];
-} file_manager_read_file_partial_response_t;
-
-/*
  * awm signaling
  */
 
@@ -54,55 +14,157 @@ typedef struct file_manager_read_file_partial_response {
 // Prior to this, awm should not request image resources, or else the request may 
 // conflict with the file manager's attempts to create a window.
 // TODO(PT): Think about ways to prevent this. Perhaps amc messages can be popped with a predicate
-#define FILE_MANAGER_READY  103
+#define FILE_MANAGER_READY  100
 
 /*
- * Launch a program by path
+ * Create File/Directory
  */
 
 // Sent from clients to the file manager
-#define FILE_MANAGER_LAUNCH_FILE 104
-typedef struct file_manager_launch_file_request {
-    uint32_t event; // FILE_MANAGER_LAUNCH_FILE
+#define FILE_MANAGER_CREATE_FILE 101
+typedef struct file_manager_create_file_request {
+    uint32_t event; // Expects FILE_MANAGER_CREATE_FILE 
     char path[FILE_MANAGER_MAX_PATH_LENGTH];
-} file_manager_launch_file_request_t;
+} file_manager_create_file_request_t;
 
-/*
- * Create Directory
- */
+// Sent from the file manager to clients
+#define FILE_MANAGER_CREATE_FILE_RESPONSE 101
+typedef struct file_manager_create_file_response {
+    uint32_t event; // Expects FILE_MANAGER_CREATE_FILE_RESPONSE
+    bool success;
+} file_manager_create_file_response_t;
 
 // Sent from clients to the file manager
-#define FILE_MANAGER_CREATE_DIRECTORY 105
+#define FILE_MANAGER_CREATE_DIRECTORY 102
 typedef struct file_manager_create_directory_request {
-    uint32_t event; // FILE_MANAGER_CREATE_DIRECTORY 
+    uint32_t event; // Expects FILE_MANAGER_CREATE_DIRECTORY 
     char path[FILE_MANAGER_MAX_PATH_LENGTH];
 } file_manager_create_directory_request_t;
 
 // Sent from the file manager to clients
-#define FILE_MANAGER_CREATE_DIRECTORY_RESPONSE 105
+#define FILE_MANAGER_CREATE_DIRECTORY_RESPONSE 102
 typedef struct file_manager_create_directory_response {
-    uint32_t event; // FILE_MANAGER_CREATE_DIRECTORY_RESPONSE
+    uint32_t event; // Expects FILE_MANAGER_CREATE_DIRECTORY_RESPONSE
     bool success;
 } file_manager_create_directory_response_t;
+
+/*
+ * Delete File/Directory
+ */
+
+// Sent from clients to the file manager
+#define FILE_MANAGER_DELETE_FILE 103
+typedef struct file_manager_delete_file_request {
+    uint32_t event; // Expects FILE_MANAGER_DELETE_FILE 
+    char path[FILE_MANAGER_MAX_PATH_LENGTH];
+} file_manager_delete_file_request_t;
+
+// Sent from the file manager to clients
+#define FILE_MANAGER_DELETE_FILE_RESPONSE 103
+typedef struct file_manager_delete_file_response {
+    uint32_t event; // Expects FILE_MANAGER_DELETE_FILE_RESPONSE
+    bool success;
+} file_manager_delete_file_response_t;
+
+// Sent from clients to the file manager
+#define FILE_MANAGER_DELETE_DIRECTORY 104
+typedef struct file_manager_delete_directory_request {
+    uint32_t event; // Expects FILE_MANAGER_DELETE_DIRECTORY 
+    char path[FILE_MANAGER_MAX_PATH_LENGTH];
+} file_manager_delete_directory_request_t;
+
+// Sent from the file manager to clients
+#define FILE_MANAGER_DELETE_DIRECTORY_RESPONSE 104
+typedef struct file_manager_delete_directory_response {
+    uint32_t event; // Expects FILE_MANAGER_DELETE_DIRECTORY_RESPONSE
+    bool success;
+} file_manager_delete_directory_response_t;
+
+/*
+ * Read a file
+ */
+
+// Sent from clients to the file manager
+#define FILE_MANAGER_READ_FILE 105
+typedef struct file_manager_read_file_request {
+    uint32_t event; // Expects FILE_MANAGER_READ_FILE 
+    char path[FILE_MANAGER_MAX_PATH_LENGTH];
+} file_manager_read_file_request_t;
+
+// Sent from the file manager to clients
+#define FILE_MANAGER_READ_FILE_RESPONSE 105
+typedef struct file_manager_read_file_response {
+    uint32_t event; // Expects FILE_MANAGER_READ_FILE_RESPONSE
+    uint32_t file_size;
+    uint8_t file_data[];
+} file_manager_read_file_response_t;
+
+// Sent from clients to the file manager
+#define FILE_MANAGER_READ_FILE__PARTIAL 106
+typedef struct file_manager_read_file_partial_request {
+    uint32_t event; // Expects FILE_MANAGER_READ_FILE 
+    uint32_t offset;
+    uint32_t length;
+    char path[FILE_MANAGER_MAX_PATH_LENGTH];
+} file_manager_read_file_partial_request_t;
+
+// Sent from the file manager to clients
+#define FILE_MANAGER_READ_FILE__PARTIAL_RESPONSE 106
+typedef struct file_manager_read_file_partial_response {
+    uint32_t event; // Expects FILE_MANAGER_READ_FILE__PARTIAL_RESPONSE
+    uint32_t data_length;
+    uint8_t file_data[];
+} file_manager_read_file_partial_response_t;
+
+/*
+ * Write a file
+ */
+
+// Sent from the file manager to clients
+#define FILE_MANAGER_WRITE_FILE 107
+typedef struct file_manager_write_file {
+    uint32_t event; // Expects FILE_MANAGER_WRITE_FILE
+    uint32_t file_offset;
+    uint32_t data_length;
+    uint8_t file_data[];
+} file_manager_write_file_t;
+
+// Sent from the file manager to clients
+#define FILE_MANAGER_WRITE_FILE_RESPONSE 107
+typedef struct file_manager_write_file_response {
+    uint32_t event; // Expects FILE_MANAGER_WRITE_FILE_RESPONSE
+    uint32_t file_length;
+} file_manager_write_file_response_t;
 
 /*
  * Check if File Exists
  */
 
 // Sent from clients to the file manager
-#define FILE_MANAGER_CHECK_FILE_EXISTS 106
+#define FILE_MANAGER_CHECK_FILE_EXISTS 108
 typedef struct file_manager_check_file_exists {
-    uint32_t event; // FILE_MANAGER_CHECK_FILE_EXISTS 
+    uint32_t event; // Expects FILE_MANAGER_CHECK_FILE_EXISTS 
     char path[FILE_MANAGER_MAX_PATH_LENGTH];
 } file_manager_check_file_exists_request_t;
 
 // Sent from the file manager to clients
-#define FILE_MANAGER_CHECK_FILE_EXISTS_RESPONSE 106
+#define FILE_MANAGER_CHECK_FILE_EXISTS_RESPONSE 108
 typedef struct file_manager_check_file_exists_response {
-    uint32_t event; // FILE_MANAGER_CHECK_FILE_EXISTS_RESPONSE
+    uint32_t event; // Expects FILE_MANAGER_CHECK_FILE_EXISTS_RESPONSE
     char path[FILE_MANAGER_MAX_PATH_LENGTH];
     bool file_exists;
     uint32_t file_size;
 } file_manager_check_file_exists_response_t;
+
+/*
+ * Launch a program by path
+ */
+
+// Sent from clients to the file manager
+#define FILE_MANAGER_LAUNCH_FILE 109
+typedef struct file_manager_launch_file_request {
+    uint32_t event; // Expects FILE_MANAGER_LAUNCH_FILE
+    char path[FILE_MANAGER_MAX_PATH_LENGTH];
+} file_manager_launch_file_request_t;
 
 #endif
