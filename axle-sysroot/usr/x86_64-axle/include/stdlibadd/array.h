6c11c9443936b0ce77fce1bf8e520ede1b5bba52
Phillip Tennen <pt@datatheorem.io>
2021-10-22 00:07:59 +0200

    [x86_64] Check in 64b sysroot

---
+++ axle-sysroot/usr/x86_64-axle/include/stdlibadd/array.h
@@ -0,0 +1,28 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+#include <stdint.h>
+
+#define ARR_NOT_FOUND -1
+typedef void* type_t;
+
+typedef struct array {
+	type_t* array;
+	int32_t size;
+	int32_t max_size;
+} array_t;
+
+array_t* array_create(int32_t max_size);
+void array_destroy(array_t* array);
+
+void array_insert(array_t* array, type_t item);
+void array_set(array_t* array, int32_t i, type_t item);
+void array_remove(array_t* array, int32_t i);
+
+type_t array_lookup(array_t* array, int32_t i);
+int32_t array_index(array_t* array, type_t item);
+
+// Helper destructor for a common code pattern that frees each element, then calls destroy
+void array_free_each_element_and_destroy(array_t* array);
+
+#endif
