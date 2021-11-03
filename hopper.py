d74f98d23a384a66ae5c3212f56253a9a1a18699
Phillip Tennen <pt@datatheorem.io>
2021-11-03 23:50:41 +0100

    [Toolchain] hopper.py has a --kernel option

--- hopper.py
+++ hopper.py
@@ -1,9 +1,22 @@
 import subprocess
+import argparse
 import sys
 
 
 def main():
-    subprocess.run(f"hopperv4 -e ./initrd/{sys.argv[1]}", shell=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--program', action="store")
+    parser.add_argument("--kernel", action="store_true")
+    parser.set_defaults(kernel=False)
+    args = parser.parse_args()
+
+    if args.kernel:
+        path = './isodir/boot/axle.bin'
+    else:
+        path = f'./initrd/{args.program}'
+
+
+    subprocess.run(f"hopperv4 -e {path}", shell=True)
 
 
 if __name__ == "__main__":
