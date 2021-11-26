a8662b9fc81a9e2378e2c0f43f8dcf7b32efc103
Phillip Tennen <phillip@axleos.com>
2021-11-26 01:36:20 +0100

    [Term] Initial terminal draft

--- programs/libport/libport.h
+++ programs/libport/libport.h
@@ -3,16 +3,6 @@
 
 #include <stdbool.h>
 
-#define max(a,b) \
-   ({ __typeof__ (a) _a = (a); \
-       __typeof__ (b) _b = (b); \
-     _a > _b ? _a : _b; })
-
-#define min(a,b) \
-   ({ __typeof__ (a) _a = (a); \
-       __typeof__ (b) _b = (b); \
-     _a <= _b ? _a : _b; })
-
 void assert(bool cond, const char* msg);
 
 void outb(uint16_t port, uint8_t val);
