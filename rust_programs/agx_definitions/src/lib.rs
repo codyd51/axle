ff9f02beb7da64607b725b6d93a7f9383f748448
Phillip Tennen <pt@datatheorem.io>
2022-01-06 20:27:32 +0100

    [Rust] Implement convenience methods for basic graphics types

--- rust_programs/agx_definitions/src/lib.rs
+++ rust_programs/agx_definitions/src/lib.rs
@@ -1,6 +1,8 @@
 #![no_std]
 
-use core::ops::Add;
+extern crate alloc;
+use alloc::vec::Vec;
+use core::ops::{Add, Sub};
 
 pub struct Color {
     pub r: u8,
@@ -11,6 +13,18 @@ pub struct Color {
 impl Color {
     pub fn new(r: u8, g: u8, b: u8) -> Self {
         Color { r, g, b }
+    }
+    pub fn from(vals: [u8; 3]) -> Self {
+        Color::new(vals[0], vals[1], vals[2])
+    }
+    pub fn white() -> Self {
+        Color::from([255, 255, 255])
+    }
+    pub fn gray() -> Self {
+        Color::from([127, 127, 127])
+    }
+    pub fn black() -> Self {
+        Color::from([0, 0, 0])
     }
 }
 
@@ -56,6 +70,47 @@ impl Size {
     }
 }
 
+impl Add for Size {
+    type Output = Size;
+    fn add(self, rhs: Self) -> Self::Output {
+        Size {
+            width: self.width + rhs.width,
+            height: self.height + rhs.height,
+        }
+    }
+}
+
+impl Sub for Size {
+    type Output = Size;
+    fn sub(self, rhs: Self) -> Self::Output {
+        Size {
+            width: self.width - rhs.width,
+            height: self.height - rhs.height,
+        }
+    }
+}
+
+// For FFI
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct PointU32 {
+    pub x: u32,
+    pub y: u32,
+}
+
+impl PointU32 {
+    pub fn new(x: u32, y: u32) -> Self {
+        PointU32 { x, y }
+    }
+
+    pub fn from(point: &Point) -> Self {
+        PointU32 {
+            x: point.x as u32,
+            y: point.y as u32,
+        }
+    }
+}
+
 #[derive(Debug, Clone, Copy)]
 pub struct Point {
     pub x: usize,
@@ -66,6 +121,15 @@ impl Point {
     pub fn new(x: usize, y: usize) -> Self {
         Point { x, y }
     }
+    pub fn zero() -> Self {
+        Point { x: 0, y: 0 }
+    }
+    pub fn from(point: &PointU32) -> Self {
+        Point {
+            x: point.x.try_into().unwrap(),
+            y: point.y.try_into().unwrap(),
+        }
+    }
 }
 
 impl Add for Point {
@@ -75,6 +139,68 @@ impl Add for Point {
             x: self.x + rhs.x,
             y: self.y + rhs.y,
         }
+    }
+}
+
+impl Sub for Point {
+    type Output = Point;
+    fn sub(self, rhs: Self) -> Self::Output {
+        Point {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Rect {
+    pub origin: Point,
+    pub size: Size,
+}
+
+impl Rect {
+    pub fn new(origin: Point, size: Size) -> Self {
+        Rect { origin, size }
+    }
+
+    pub fn inset_by(&self, bottom: usize, left: usize, right: usize, top: usize) -> Self {
+        // TODO(PT): Handle underflow by making Size signed? or handle explicitly here?
+        Rect::new(
+            self.origin + Point::new(left, top),
+            self.size - Size::new(right * 2, bottom * 2),
+        )
+    }
+
+    pub fn min_x(&self) -> usize {
+        self.origin.x
+    }
+
+    pub fn min_y(&self) -> usize {
+        self.origin.y
+    }
+
+    pub fn max_x(&self) -> usize {
+        self.min_x() + self.size.width
+    }
+
+    pub fn max_y(&self) -> usize {
+        self.min_y() + self.size.height
+    }
+
+    pub fn mid_x(&self) -> usize {
+        self.min_x() + ((self.size.width as f64 / 2f64) as usize)
+    }
+
+    pub fn mid_y(&self) -> usize {
+        self.min_y() + ((self.size.height as f64 / 2f64) as usize)
+    }
+
+    pub fn center(&self) -> Point {
+        Point::new(self.mid_x(), self.mid_y())
+    }
+
+    pub fn contains(&self, p: Point) -> bool {
+        p.x >= self.min_x() && p.y >= self.min_y() && p.x < self.max_x() && p.y < self.max_y()
     }
 }
 
