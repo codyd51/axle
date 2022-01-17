b78b551c2ff89d9fb921234ab532e55eab5a4c86
Phillip Tennen <pt@datatheorem.io>
2022-01-17 01:17:15 +0100

    [Rust] Check in file browser state

--- rust_programs/fs_client/src/main.rs
+++ rust_programs/fs_client/src/main.rs
@@ -19,10 +19,13 @@ use view::View;
 
 use axle_rt::{amc_message_await, amc_message_send, amc_register_service, printf, AmcMessage};
 
-use agx_definitions::{Color, Drawable, LayerSlice, NestedLayerSlice, Point, Rect, Size};
+use agx_definitions::{
+    Color, Drawable, LayerSlice, NestedLayerSlice, Point, Rect, Size, StrokeThickness,
+};
 
 use file_manager_messages::{
-    str_from_u8_nul_utf8_unchecked, FileManagerDirectoryContents, FileManagerReadDirectory,
+    str_from_u8_nul_utf8_unchecked, FileManagerDirectoryContents, FileManagerDirectoryEntry,
+    FileManagerReadDirectory,
 };
 
 mod bordered;
@@ -39,28 +42,28 @@ use window::AwmWindow;
 
 fn select_current_path_view_height(superview_size: Size) -> isize {
     let min_height = 100;
-    let divisor_for_top_view = 8;
+    let divisor_for_top_view = 6;
     cmp::max(min_height, superview_size.height / divisor_for_top_view)
 }
 
 struct CurrentPathView {
-    view: View,
+    view: Rc<View>,
     current_path_label: RefCell<Rc<Label>>,
     pub current_path: RefCell<String>,
 }
 
 impl CurrentPathView {
     pub fn new<F: 'static + Fn(&View, Size) -> Rect>(sizer: F) -> Self {
-        let view = View::new(Color::light_gray(), sizer);
+        let view = Rc::new(View::new(Color::light_gray(), sizer));
 
-        let initial_path = "/";
+        let initial_path = "/usr/";
         let current_path_label = Rc::new(Label::new(
-            Rect::new(10, 10, 200, 16),
+            Rect::new(10, 10, 400, 16),
             &format!("Current path: {}", initial_path),
             Color::new(30, 30, 30),
         ));
         let current_path_label_clone = Rc::clone(&current_path_label);
-        view.add_component(current_path_label_clone);
+        Rc::clone(&view).add_component(current_path_label_clone);
 
         let back_button = Rc::new(Button::new(
             Rect::from_parts(
@@ -70,7 +73,7 @@ impl CurrentPathView {
             "Go Back",
         ));
         let back_button_clone = Rc::clone(&back_button);
-        view.add_component(back_button_clone);
+        Rc::clone(&view).add_component(back_button_clone);
 
         CurrentPathView {
             view: view,
@@ -108,8 +111,13 @@ impl Drawable for CurrentPathView {
         self.view.frame()
     }
 
-    fn draw(&self, onto: &mut LayerSlice) {
-        Bordered::draw(self, onto)
+    fn content_frame(&self) -> Rect {
+        //self.view.content_frame()
+        Bordered::content_frame(self)
+    }
+
+    fn draw(&self) {
+        Bordered::draw(self)
     }
 }
 
@@ -117,19 +125,179 @@ impl Bordered for CurrentPathView {
     fn draw_inner_content(&self, outer_frame: Rect, onto: &mut LayerSlice) {
         self.view.draw_inner_content(outer_frame, onto);
     }
+
+    fn set_interior_content_frame(&self, inner_content_frame: Rect) {
+        self.view.set_interior_content_frame(inner_content_frame)
+    }
+
+    fn get_interior_content_frame(&self) -> Rect {
+        self.view.get_interior_content_frame()
+    }
 }
 
 impl UIElement for CurrentPathView {
-    fn handle_mouse_entered(&self, onto: &mut LayerSlice) {
-        self.view.handle_mouse_entered(onto)
+    fn handle_mouse_entered(&self) {
+        self.view.handle_mouse_entered()
     }
 
-    fn handle_mouse_exited(&self, onto: &mut LayerSlice) {
-        self.view.handle_mouse_exited(onto)
+    fn handle_mouse_exited(&self) {
+        self.view.handle_mouse_exited()
     }
 
-    fn handle_mouse_moved(&self, mouse_point: Point, onto: &mut LayerSlice) {
-        self.view.handle_mouse_moved(mouse_point, onto)
+    fn handle_mouse_moved(&self, mouse_point: Point) {
+        self.view.handle_mouse_moved(mouse_point)
+    }
+
+    fn handle_left_click(&self) {
+        self.view.handle_left_click()
+    }
+
+    fn handle_superview_resize(&self, superview_size: Size) {
+        self.view.handle_superview_resize(superview_size)
+    }
+
+    fn currently_contains_mouse(&self) -> bool {
+        self.view.currently_contains_mouse()
+    }
+}
+
+struct DirectoryEntryView {
+    view: Rc<View>,
+    entry: FileManagerDirectoryEntry,
+    background_color: Color,
+}
+
+impl DirectoryEntryView {
+    pub fn new<F: 'static + Fn(&View, Size) -> Rect>(
+        entry_index: usize,
+        entry: FileManagerDirectoryEntry,
+        sizer: F,
+    ) -> Self {
+        let background_color = match entry_index % 2 {
+            0 => Color::new(140, 140, 140),
+            1 => Color::new(120, 120, 120),
+            _ => panic!("Should never happen"),
+        };
+        let view = Rc::new(View::new(background_color, sizer));
+        view.set_border_enabled(false);
+
+        let label_width = 200;
+        let button_width = 60;
+        let height = 30;
+
+        let entry_name = str_from_u8_nul_utf8_unchecked(&entry.name);
+        let label_suffix = match entry.is_directory {
+            true => "/",
+            false => "",
+        };
+        let label_text = format!("{entry_name}{label_suffix}");
+        let name_label = Rc::new(Label::new(
+            Rect::from_parts(Point::new(10, 10), Size::new(300, height)),
+            &label_text,
+            Color::new(30, 30, 30),
+        ));
+        // TODO(PT): Set font size as attribute?
+        let label_width = 8 * name_label.text.borrow().len() as isize;
+        Rc::clone(&view).add_component(name_label);
+
+        let button_text = match entry.is_directory {
+            true => "Browse",
+            false => "Open",
+        };
+        let button = Rc::new(Button::new(
+            Rect::from_parts(Point::new(400, 1), Size::new(button_width, height - 4)),
+            button_text,
+        ));
+        let button_clone = Rc::clone(&button);
+        Rc::clone(&view).add_component(button_clone);
+
+        DirectoryEntryView {
+            view,
+            entry,
+            background_color,
+        }
+    }
+}
+
+impl NestedLayerSlice for DirectoryEntryView {
+    fn get_parent(&self) -> Option<Weak<dyn NestedLayerSlice>> {
+        self.view.get_parent()
+    }
+
+    fn set_parent(&self, parent: Weak<dyn NestedLayerSlice>) {
+        self.view.set_parent(parent);
+    }
+
+    fn get_slice(&self) -> LayerSlice {
+        self.view.get_slice()
+    }
+}
+
+impl Drawable for DirectoryEntryView {
+    fn frame(&self) -> Rect {
+        self.view.frame()
+    }
+
+    fn content_frame(&self) -> Rect {
+        Bordered::content_frame(self)
+    }
+
+    fn draw(&self) {
+        Bordered::draw(self)
+    }
+}
+
+impl Bordered for DirectoryEntryView {
+    fn draw_border(&self) -> Rect {
+        let onto = self.get_slice();
+        let border_rect = Rect::from_parts(Point::zero(), onto.frame.size);
+
+        let border_color = match self.currently_contains_mouse() {
+            //true => Color::white(),
+            true => Color::new(20, 80, 160),
+            false => self.background_color,
+        };
+        let border_thickness = 1;
+        onto.fill_rect(
+            border_rect,
+            border_color,
+            StrokeThickness::Width(border_thickness),
+        );
+        let inner_content_rect = border_rect.inset_by(
+            border_thickness,
+            border_thickness,
+            border_thickness,
+            border_thickness,
+        );
+        inner_content_rect
+    }
+
+    fn draw_inner_content(&self, outer_frame: Rect, onto: &mut LayerSlice) {
+        self.view.draw_inner_content(outer_frame, onto);
+    }
+
+    fn set_interior_content_frame(&self, inner_content_frame: Rect) {
+        self.view.set_interior_content_frame(inner_content_frame)
+    }
+
+    fn get_interior_content_frame(&self) -> Rect {
+        self.view.get_interior_content_frame()
+    }
+}
+
+impl UIElement for DirectoryEntryView {
+    fn handle_mouse_entered(&self) {
+        self.view.handle_mouse_entered();
+        self.draw_border();
+    }
+
+    fn handle_mouse_exited(&self) {
+        self.view.handle_mouse_exited();
+        self.draw_border();
+    }
+
+    fn handle_mouse_moved(&self, mouse_point: Point) {
+        self.view.handle_mouse_moved(mouse_point)
     }
 
     fn handle_left_click(&self) {
@@ -146,13 +314,13 @@ impl UIElement for CurrentPathView {
 }
 
 struct DirectoryContentsView {
-    view: View,
+    view: Rc<View>,
     path_to_button_map: BTreeMap<String, Rc<Button>>,
 }
 
 impl DirectoryContentsView {
     pub fn new<F: 'static + Fn(&View, Size) -> Rect>(path: &str, sizer: F) -> Self {
-        let view = View::new(Color::new(170, 170, 170), sizer);
+        let view = Rc::new(View::new(Color::new(170, 170, 170), sizer));
 
         let fs_server = "com.axle.file_manager2";
         // TODO(PT): Should return the normalized path (ie strip extra slashes and normalize ../)
@@ -161,32 +329,62 @@ impl DirectoryContentsView {
             amc_message_await(Some(fs_server));
 
         let mut cursor = Point::new(10, 10);
-        let button_height = 30;
+        let entry_height = 30;
 
         let mut path_to_button_map = BTreeMap::new();
 
-        for entry in dir_contents
+        for (i, entry) in dir_contents
             .body()
             .entries
             .iter()
             .filter_map(|e| e.as_ref())
+            .enumerate()
         {
             let entry_name = str_from_u8_nul_utf8_unchecked(&entry.name);
-
-            let button_width = 100;
-            let button = Rc::new(Button::new(
-                Rect::from_parts(cursor, Size::new(button_width, button_height)),
-                entry_name,
+            let entry_view = Rc::new(DirectoryEntryView::new(
+                i,
+                *entry,
+                move |_dir_entry_view, superview_size| {
+                    Rect::from_parts(
+                        Point::new(0, (i * entry_height).try_into().unwrap()),
+                        Size::new(superview_size.width, entry_height.try_into().unwrap()),
+                    )
+                },
             ));
-            let button_clone = Rc::clone(&button);
-            view.add_component(button_clone);
+            printf!("Adding DirectoryEntryView...\n");
+            Rc::clone(&view).add_component(entry_view);
+            printf!("Added DirectoryEntryView\n");
 
-            path_to_button_map.insert(entry_name.to_string(), button);
+            /*
+            let label_width = 200;
+            let button_width = 60;
+            let name_label = Rc::new(Label::new(
+                Rect::from_parts(cursor, Size::new(label_width, entry_height)),
+                entry_name,
+                Color::black(),
+            ));
+            Rc::clone(&view).add_component(name_label);
+            */
 
-            cursor.y += button_height + 20;
+            /*
+            if entry.is_directory {
+                let button = Rc::new(Button::new(
+                    Rect::from_parts(
+                        Point::new(cursor.x + label_width, cursor.y),
+                        Size::new(button_width, entry_height),
+                    ),
+                    "Browse",
+                ));
+                let button_clone = Rc::clone(&button);
+                Rc::clone(&view).add_component(button_clone);
+                path_to_button_map.insert(entry_name.to_string(), button);
+            }
+
+            cursor.y += entry_height + 20;
             if cursor.y > 500 {
                 break;
             }
+            */
         }
 
         DirectoryContentsView {
@@ -201,8 +399,13 @@ impl Drawable for DirectoryContentsView {
         self.view.frame()
     }
 
-    fn draw(&self, onto: &mut LayerSlice) {
-        Bordered::draw(self, onto)
+    fn content_frame(&self) -> Rect {
+        //self.view.content_frame()
+        Bordered::content_frame(self)
+    }
+
+    fn draw(&self) {
+        Bordered::draw(self);
     }
 }
 
@@ -210,19 +413,27 @@ impl Bordered for DirectoryContentsView {
     fn draw_inner_content(&self, outer_frame: Rect, onto: &mut LayerSlice) {
         self.view.draw_inner_content(outer_frame, onto);
     }
+
+    fn set_interior_content_frame(&self, inner_content_frame: Rect) {
+        self.view.set_interior_content_frame(inner_content_frame)
+    }
+
+    fn get_interior_content_frame(&self) -> Rect {
+        self.view.get_interior_content_frame()
+    }
 }
 
 impl UIElement for DirectoryContentsView {
-    fn handle_mouse_entered(&self, onto: &mut LayerSlice) {
-        self.view.handle_mouse_entered(onto)
+    fn handle_mouse_entered(&self) {
+        self.view.handle_mouse_entered()
     }
 
-    fn handle_mouse_exited(&self, onto: &mut LayerSlice) {
-        self.view.handle_mouse_exited(onto)
+    fn handle_mouse_exited(&self) {
+        self.view.handle_mouse_exited()
     }
 
-    fn handle_mouse_moved(&self, mouse_point: Point, onto: &mut LayerSlice) {
-        self.view.handle_mouse_moved(mouse_point, onto)
+    fn handle_mouse_moved(&self, mouse_point: Point) {
+        self.view.handle_mouse_moved(mouse_point)
     }
 
     fn handle_left_click(&self) {
@@ -323,14 +534,12 @@ impl FileBrowser2 {
 
                 // Redraw the status bar since the current path has updated
                 let path_view = &**browser_clone2.current_path_view.borrow();
-                let mut slice = path_view.get_slice();
-                Bordered::draw(path_view, &mut slice);
+                Bordered::draw(path_view);
 
                 // Redraw the contents view as we've got new directory contents to display
                 let contents_view_container = browser_clone2.directory_contents_view.borrow();
                 let contents_view = &**contents_view_container.as_ref().unwrap();
-                let mut slice2 = contents_view.get_slice();
-                Bordered::draw(contents_view, &mut slice2);
+                Bordered::draw(contents_view);
 
                 window_clone.commit();
             });
@@ -382,7 +591,7 @@ fn start(_argc: isize, _argv: *const *const u8) -> isize {
     amc_register_service("com.axle.fs2_client");
 
     // TODO(PT): Add a main content view to Window?
-    let window = Rc::new(AwmWindow::new("Rust File Manager", Size::new(500, 800)));
+    let window = Rc::new(AwmWindow::new("Rust File Manager", Size::new(500, 600)));
     let _file_browser = Rc::new(RefCell::new(FileBrowser2::new(Rc::clone(&window))));
 
     window.enter_event_loop();
