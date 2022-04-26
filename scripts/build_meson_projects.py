e29f32a5380f9383ee249afe85bf53a96e13ee36
Phillip Tennen <pt@datatheorem.io>
2022-04-26 16:19:33 +0100

    [CI] Tweak meson build script

--- scripts/build_meson_projects.py
+++ scripts/build_meson_projects.py
@@ -18,11 +18,6 @@ def build_meson_projects(only_build: Optional[List[str]] = None) -> None:
         print(f"Forcing rebuild of {programs_root}...")
         run_and_check(["rm", "-rf", build_folder.as_posix()])
 
-    if not build_folder.exists():
-        print(f"Running one-time Meson configuration in {programs_root}...")
-        cross_compile_config_path = generate_meson_cross_file_if_necessary()
-        run_and_check(["meson", "build", "--cross-file", cross_compile_config_path.as_posix()], cwd=programs_root)
-
     if only_build:
         for subdir_name in only_build:
             subdir = programs_root / "subprojects" / subdir_name
@@ -30,8 +25,14 @@ def build_meson_projects(only_build: Optional[List[str]] = None) -> None:
             run_and_check(["meson", "compile", "-C", "build"], cwd=subdir)
             run_and_check(["meson", "install", "-C", "build"], cwd=subdir)
     else:
+        if not build_folder.exists():
+            print(f"Running one-time Meson configuration in {programs_root}...")
+            cross_compile_config_path = generate_meson_cross_file_if_necessary()
+            run_and_check(["meson", "build", "--cross-file", cross_compile_config_path.as_posix()], cwd=programs_root)
+
         run_and_check(["meson", "compile", "-C", "build"], cwd=programs_root)
-        run_and_check(["meson", "install", "-C", "build", "--only-changed"], cwd=programs_root)
+        # run_and_check(["meson", "install", "-C", "build", "--only-changed"], cwd=programs_root)
+        run_and_check(["meson", "install", "-C", "build"], cwd=programs_root)
 
 
 if __name__ == "__main__":
