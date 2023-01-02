77dd81be36d15fa20ad972436657c3208a0a7212
Phillip Tennen <pt@datatheorem.io>
2023-01-02 02:42:30 +0000

    [Kernel] Flesh out basic Rust-side FFI support

--- rust_kernel_libs/ffi_bindings/src/lib.rs
+++ rust_kernel_libs/ffi_bindings/src/lib.rs
@@ -1,4 +1,5 @@
 #![no_std]
+#![feature(format_args_nl)]
 #![feature(panic_info_message)]
 #![feature(default_alloc_error_handler)]
 
@@ -7,33 +8,77 @@ extern crate alloc;
 use alloc::alloc::{GlobalAlloc, Layout};
 use alloc::format;
 use alloc::string::ToString;
+use core::ffi::{c_char, c_void};
 use core::panic::PanicInfo;
+pub use cstr_core;
+use cstr_core::CString;
 
-#[no_mangle]
-pub fn add2(left: usize, right: usize) -> usize {
-    left + right
+extern "C" {
+    pub fn kmalloc(size: usize) -> *mut c_void;
+    pub fn kfree(ptr: *mut c_char);
+    pub fn printf(fmt: *const u8, ...) -> i32;
+    pub fn assert(condition: bool, message: *const u8) -> ();
+    pub fn _panic(message: *const u8, file: *const u8, line: i64);
+}
+
+#[macro_export]
+macro_rules! printf {
+    ($($arg:tt)*) => ({
+        let s = alloc::fmt::format(core::format_args!($($arg)*));
+        for x in s.split('\0') {
+            let log = ffi_bindings::cstr_core::CString::new(x).expect("printf format failed");
+            unsafe { ffi_bindings::printf(log.as_ptr() as *const u8); }
+        }
+    })
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::printf!("\n"));
+    ($($arg:tt)*) => ({
+        let s = alloc::fmt::format(core::format_args_nl!($($arg)*));
+        for x in s.split('\0') {
+            let log = ffi_bindings::cstr_core::CString::new(x).expect("printf format failed");
+            unsafe { ffi_bindings::printf(log.as_ptr() as *const u8); }
+        }
+    })
+}
+
+macro_rules! internal_println {
+    () => ($crate::printf!("\n"));
+    ($($arg:tt)*) => ({
+        let s = alloc::fmt::format(core::format_args_nl!($($arg)*));
+        for x in s.split('\0') {
+            let log = ::cstr_core::CString::new(x).expect("printf format failed");
+            unsafe { printf(log.as_ptr() as *const u8); }
+        }
+    })
 }
 
 #[panic_handler]
-fn panic(panic_info: &PanicInfo<'_>) -> ! {
+pub fn panic(panic_info: &PanicInfo<'_>) -> ! {
     let msg = match panic_info.message() {
         Some(s) => format!("{}", s),
         None => "Box<Any>".to_string(),
     };
 
     if let Some(location) = panic_info.location() {
-        /*
         internal_println!(
             "panic occurred in file '{}' at line {}",
             location.file(),
             location.line(),
         );
-        */
     }
 
-    //let c_to_print = CString::new(msg).expect("CString::new failed");
+    let c_to_print = CString::new(msg).expect("CString::new failed");
+    let filename = CString::new("Rust file").unwrap();
+    unsafe { printf(c_to_print.as_ptr() as *const u8) };
     unsafe {
-        //libc::assert(false, c_to_print.as_ptr() as *const u8);
+        _panic(
+            c_to_print.as_ptr() as *const u8,
+            filename.as_ptr() as *const u8,
+            0,
+        );
     }
     loop {}
 }
@@ -42,27 +87,12 @@ pub struct Dummy;
 
 unsafe impl GlobalAlloc for Dummy {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        /*
-        libc::printf(
-            cstr_core::cstr!("Malloc %d\n").as_ptr() as *const u8,
-            layout.size() as u32,
-        );
-        */
-        //printf!("Malloc {}\n", layout.size());
-        //libc::malloc(layout.size()) as *mut u8
-        todo!()
+        kmalloc(layout.size()) as *mut u8
     }
-    unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
-        /*
-        libc::printf(
-            cstr_core::cstr!("Free %d\n").as_ptr() as *const u8,
-            _layout.size() as u32,
-        );
-        */
-        //libc::free(ptr as *mut libc::c_char);
-        todo!()
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        kfree(ptr as *mut c_char);
     }
 }
 
 #[global_allocator]
-static ALLOCATOR: Dummy = Dummy;
+pub static ALLOCATOR: Dummy = Dummy;
