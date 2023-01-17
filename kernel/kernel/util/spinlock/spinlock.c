412181244bc4aaf58e2f215c76daf514e5f5d74c
Phillip Tennen <pt@datatheorem.io>
2023-01-17 21:01:17 +0000

    [Kernel] Reimplement spinlock

--- kernel/kernel/util/spinlock/spinlock.c
+++ kernel/kernel/util/spinlock/spinlock.c
@@ -14,16 +14,86 @@ static inline int atomic_add_fetch(int* ptr, int d) {
     return __atomic_add_fetch(ptr, d, __ATOMIC_SEQ_CST);
 }
 
+static inline uint8_t atomic_bit_test_and_set(uint32_t* val) {
+    uint8_t ret;
+    asm volatile (
+            "LOCK bts $0, %0;"
+            "     setc    %1;"
+            : "+m" (*val), "=qm" (ret)
+            :
+            : "cc", "memory");
+    return ret;
+}
+
+static inline void local_irq_disable(void) {
+    asm volatile (
+        "cli"
+        ::
+        :"cc", "memory"
+    );
+}
+
+static inline void local_irq_enable(void) {
+    asm volatile (
+        "sti"
+        ::
+        :"cc", "memory"
+    );
+}
+
+static inline union x86_rflags get_rflags(void) {
+    union x86_rflags flags;
+    asm volatile(
+        "pushfq;"
+        "popq %0;"
+        :"=rm"(flags.raw)
+        :
+    );
+    return flags;
+}
+
+static inline void set_rflags(union x86_rflags flags) {
+    asm volatile(
+        "pushq %0;"
+        "popfq;"
+        :
+        :"g"(flags.raw)
+        :"cc", "memory"
+    );
+}
+
+/*
+ * Disable interrupts, but restore the original %rflags
+ * interrupt enable flag (IF) state afterwards.
+ */
+
+static inline union x86_rflags local_irq_disable_save(void) {
+    union x86_rflags flags = get_rflags();
+    if (flags.__packed.irqs_enabled) {
+        local_irq_disable();
+    }
+    return flags;
+}
+
+static inline void local_irq_restore(union x86_rflags flags) {
+    if (flags.__packed.irqs_enabled) {
+        set_rflags(flags);
+    }
+}
+
 void spinlock_acquire(spinlock_t* lock) {
     if (!lock) return;
     assert(lock->name, "Spinlock was used without assigning a name");
 
     // Keep track of whether we had to wait to acquire the lock
+    /*
 	uint32_t contention_start = 0;
 
 	if (lock->flag != 0) {
         // Check for data corruption
-		assert(lock->flag == 1, "Bad lock flag");
+        if (lock->flag != 1) {
+            assert(lock->flag == 1, "Bad lock flag");
+        }
 
         // We should never be waiting on a spinlock while interrupts are disabled
         // (This means another task has acquired the spinlock, 
@@ -38,7 +108,7 @@ void spinlock_acquire(spinlock_t* lock) {
                 assert(0, "Spinlock held by another process while interrupts are disabled\n");
             }
         }
-        */
+        *
 
         // If we're already holding the lock, increment the 'nested acquire' count
 		contention_start = time();
@@ -48,15 +118,28 @@ void spinlock_acquire(spinlock_t* lock) {
             return;
         }
 
-		printf("Spinlock: [%d] found contended spinlock with flag %d at %d: %s owned by %d\n", getpid(), lock->flag, contention_start, lock->name, lock->owner_pid);
-        assert(false, "contended spinlock");
+		//printf("Spinlock: [%d] found contended spinlock with flag %d at %d: %s owned by %d\n", getpid(), lock->flag, contention_start, lock->name, lock->owner_pid);
+        //assert(false, "contended spinlock");
 	}
 
     // Spin until the lock is released
+    /*
     while (!atomic_compare_exchange(&lock->flag, 0, 1)) {
-        task_switch();
+        //task_switch();
     }
+     */
+    union x86_rflags rflags = local_irq_disable_save();
+    while (atomic_bit_test_and_set(&lock->flag) == 1) {
+        local_irq_restore(rflags);
+        while (lock->flag == 1) {
+	        asm volatile ("pause":::"memory");
+        }
+        local_irq_disable();
+    }
+    lock->rflags = rflags;
+    lock->owner_pid = getpid();
 
+    /*
     // Prevent another context on this processor from acquiring the spinlock
     // atomic_store(&lock->interrupts_enabled_before_acquire, interrupts_enabled());
     lock->interrupts_enabled_before_acquire = interrupts_enabled();
@@ -69,15 +152,17 @@ void spinlock_acquire(spinlock_t* lock) {
     lock->owner_pid = getpid();
 
 	if (contention_start) {
-		printf("Spinlock: *** Proc %d received contended lock 0x%08x %s after %d ticks\n", getpid(), lock, lock->name, time() - contention_start);
+		//printf("Spinlock: *** Proc %d received contended lock 0x%08x %s after %d ticks\n", getpid(), lock, lock->name, time() - contention_start);
 	}
     else {
         //printf("Spinlock: Proc %d received uncontended lock 0x%08x %s\n", getpid(), lock, lock->name);
     }
+     */
 }
 
 void spinlock_release(spinlock_t* lock) {
     if (!lock) return;
+    /*
     if (lock->nest_count) {
         lock->nest_count -= 1;
         //printf("Spinlock: [%d] decrement nested acquire of %s to %d\n", getpid(), lock->name, lock->nest_count);
@@ -89,4 +174,9 @@ void spinlock_release(spinlock_t* lock) {
         asm("sti");
     }
     //printf("Spinlock: Proc %d freed lock 0x%08x %s\n", getpid(), lock, lock->name);
+     */
+    union x86_rflags rflags = lock->rflags;
+    asm volatile ("":::"memory");
+    lock->flag = 0;
+    local_irq_restore(rflags);
 }
--- kernel/kernel/util/spinlock/spinlock.h
+++ kernel/kernel/util/spinlock/spinlock.h
@@ -4,12 +4,41 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+union x86_rflags {
+    uint64_t raw;
+    struct {
+        uint32_t carry_flag:1,		/* Last math op resulted carry */
+        __reserved1_0:1,	/* Always 1 */
+        parity_flag:1,		/* Last op resulted even parity */
+        __reserved0_0:1,	/* Must be zero */
+        auxiliary_flag:1,	/* BCD carry */
+        __reserved0_1:1,	/* Must be zero */
+        zero_flag:1,		/* Last op resulted zero */
+        sign_flag:1,		/* Last op resulted negative */
+        trap_flag:1,		/* Enable single-step mode */
+        irqs_enabled:1,		/* Maskable interrupts enabled? */
+        direction_flag:1,	/* String ops direction */
+        overflow_flag:1,	/* Last op MSB overflowed */
+        io_privilege:2,		/* IOPL of current task */
+        nested_task:1,		/* Controls chaining of tasks */
+        __reserved0_2:1,	/* Must be zero */
+        resume_flag:1,		/* Debug exceptions related */
+        virtual_8086:1,		/* Enable/disable 8086 mode */
+        alignment_check:1,	/* Enable alignment checking? */
+        virtual:2,		/* Virtualization fields */
+        id_flag:1,		/* CPUID supported if writable */
+        __reserved0_3:10;	/* Must be zero */
+        uint32_t __reserved0_4;		/* Must be zero */
+    } __packed;
+};
+
 typedef struct spinlock_t {
-	int flag;
+	uint32_t flag;
 	char* name;
 	bool interrupts_enabled_before_acquire;
 	int owner_pid;
 	int nest_count;
+    union x86_rflags rflags;
 } spinlock_t;
 
 void spinlock_acquire(spinlock_t* lock);
