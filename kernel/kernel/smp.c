186cb1bf0b2ebba249055bd174f01600b47a2276
Phillip Tennen <pt@datatheorem.io>
2023-01-19 22:36:35 +0000

    [Kernel] Load another high-memory GDT

--- kernel/kernel/smp.c
+++ kernel/kernel/smp.c
@@ -34,20 +34,21 @@ void smp_init(void) {
     // First, verify our assumption that the AP bootstrap fits into a page
     assert(boot_info->ap_bootstrap_size < PAGE_SIZE, "AP bootstrap was larger than a page!");
 
+    /*
     printf("Copy AP bootstrap from [0x%p - 0x%p] to [0x%p - 0x%p]\n",
            boot_info->ap_bootstrap_base,
            boot_info->ap_bootstrap_base + boot_info->ap_bootstrap_size,
            AP_BOOTSTRAP_CODE_PAGE,
            AP_BOOTSTRAP_CODE_PAGE + boot_info->ap_bootstrap_size
     );
+    */
     memcpy((void*)PMA_TO_VMA(AP_BOOTSTRAP_CODE_PAGE), (void*)PMA_TO_VMA(boot_info->ap_bootstrap_base), boot_info->ap_bootstrap_size);
 
     // Set up the protected mode GDT parameter
     uintptr_t gdt_size = 0;
     gdt_descriptor_t* protected_mode_gdt = gdt_create_for_protected_mode(&gdt_size);
     // Ensure the GDT fits in the expected size
-    assert(sizeof(gdt_pointer_t) + gdt_size <= AP_BOOTSTRAP_PARAM_OFFSET_LONG_MODE_GDT, "Protected mode GDT was too big to fit in its parameter slot");
-    printf("Got protected mode gdt %p\n", protected_mode_gdt);
+    assert(sizeof(gdt_pointer_t) + gdt_size <= AP_BOOTSTRAP_PARAM_OFFSET_LONG_MODE_LOW_MEMORY_GDT, "Protected mode GDT was too big to fit in its parameter slot");
 
     gdt_pointer_t protected_mode_gdt_ptr = {0};
     uint32_t relocated_protected_mode_gdt_addr = AP_BOOTSTRAP_PARAM_PROTECTED_MODE_GDT + 8;
@@ -67,7 +68,7 @@ void smp_init(void) {
     uintptr_t ap_c_entry_point_addr = (uintptr_t)&ap_c_entry;
     memcpy((void*)PMA_TO_VMA(AP_BOOTSTRAP_PARAM_C_ENTRY), &ap_c_entry_point_addr, sizeof(ap_c_entry_point_addr));
 
-    printf("Bootloader provided RSDP 0x%x\n", boot_info->acpi_rsdp);
+    //printf("Bootloader provided RSDP 0x%x\n", boot_info->acpi_rsdp);
 
     // Parse the ACPI tables and store the SMP info in the global system state
     smp_info_t* smp_info = acpi_parse_root_system_description(boot_info->acpi_rsdp);
@@ -76,7 +77,7 @@ void smp_init(void) {
     // Set up the BSP's APIC and the IO APIC
     apic_init(boot_info->smp_info);
 
-    asm("sti");
+    //asm("sti");
 
     // Set up the BSP's per-core data structure
     for (uintptr_t i = 0; i < smp_info->processor_count; i++) {
@@ -93,36 +94,29 @@ void smp_init(void) {
 
     // Do per-core work
     for (uintptr_t i = 0; i < smp_info->processor_count; i++) {
-        printf("Booting core idx %d\n", i);
         processor_info_t* processor_info = &smp_info->processors[i];
-        printf("Processor info %d %d\n", processor_info->apic_id, processor_info->processor_id);
 
         // Skip the BSP
         if (processor_info->apic_id == smp_get_current_core_apic_id(smp_info)) {
-            printf("Skipping BSP\n");
             continue;
         }
+
+        printf("Booting core [idx %d], [APIC %d] [ID %d]\n", processor_info->apic_id, processor_info->processor_id);
 
         // Set up a virtual address space for the AP to use
         // Start off by cloning the BSP's address space, which has the high-memory remap
         // But ensure we create a new PML4E for CPU-local storage
-        printf("Cloning BSP VAS...\n");
         vas_state_t* ap_vas = vas_clone_ex(boot_info->vas_kernel, false);
-        printf("Finished clone BSP VAS!\n");
         // Identity map the low 4G. We need to identity map more than just the AP bootstrap pages because the PML4 will reference arbitrary frames.
         // TODO(PT): This will cause problems if any of the paging structures are allocated above 4GB...
         vas_map_range_exact(ap_vas, 0x0, (1024LL * 1024LL * 1024LL * 4LL), 0x0, VAS_RANGE_ACCESS_LEVEL_READ_WRITE, VAS_RANGE_PRIVILEGE_LEVEL_KERNEL);
-        printf("Finished mapping 4GB\n");
         memcpy((void*)PMA_TO_VMA(AP_BOOTSTRAP_PARAM_PML4), &ap_vas->pml4_phys, sizeof(ap_vas->pml4_phys));
 
         // Allocate a stack for the AP to use
-        printf("Allocating stack...\n");
         int ap_stack_size = PAGE_SIZE * 4;
         void* ap_stack = calloc(1, ap_stack_size);
         uintptr_t ap_stack_top = (uintptr_t)ap_stack + ap_stack_size;
         memcpy((void*)PMA_TO_VMA(AP_BOOTSTRAP_PARAM_STACK_TOP), &ap_stack_top, sizeof(ap_stack_top));
-
-        printf("Finished create stack\n");
 
         // Long mode GDT
         uintptr_t ap_long_mode_gdt_size = 0;
@@ -130,26 +124,30 @@ void smp_init(void) {
         tss_t* ap_long_mode_tss = 0;
         gdt_create_for_long_mode(&ap_long_mode_gdt, &ap_long_mode_gdt_size, &ap_long_mode_tss);
 
-        printf("Finished create long mode GDT\n");
-
         // Create a new GDT pointer that we'll place in the bootstrap params page
-        gdt_pointer_t ap_long_mode_gdt_ptr = {
-            .table_base = AP_BOOTSTRAP_PARAM_LONG_MODE_GDT + 8,
-            .table_size = ap_long_mode_gdt_size
+        gdt_pointer_t ap_long_mode_low_memory_gdt_ptr = {
+            .table_base = AP_BOOTSTRAP_PARAM_LONG_MODE_LOW_MEMORY_GDT + 8,
+            .table_size = ap_long_mode_gdt_size - 1
         };
         // Copy the pointer
-        memcpy((void*)PMA_TO_VMA(AP_BOOTSTRAP_PARAM_LONG_MODE_GDT), &ap_long_mode_gdt_ptr, sizeof(gdt_pointer_t));
+        memcpy((void*)PMA_TO_VMA(AP_BOOTSTRAP_PARAM_LONG_MODE_LOW_MEMORY_GDT), &ap_long_mode_low_memory_gdt_ptr, sizeof(gdt_pointer_t));
         // Copy the table
-        memcpy((void*)PMA_TO_VMA(AP_BOOTSTRAP_PARAM_LONG_MODE_GDT + 8), ap_long_mode_gdt, ap_long_mode_gdt_size);
+        memcpy((void*)PMA_TO_VMA(AP_BOOTSTRAP_PARAM_LONG_MODE_LOW_MEMORY_GDT + 8), ap_long_mode_gdt, ap_long_mode_gdt_size);
 
-        printf("Finished mapping gdt etc\n");
+        // Copy another GDT to a high-memory location
+        // This will be the 'final' GDT that we use
+        // We need a low-memory version because the low-memory GDT will be loaded before paging is enabled
+        // And we need a high-memory version because the low-memory identity map will be removed later
+        uintptr_t gdt_frame_phys = pmm_alloc();
+        gdt_pointer_t* ap_long_mode_high_memory_gdt_ptr = (gdt_pointer_t*)PMA_TO_VMA(gdt_frame_phys);
+        ap_long_mode_high_memory_gdt_ptr->table_base = (uintptr_t)ap_long_mode_gdt;
+        ap_long_mode_high_memory_gdt_ptr->table_size = ap_long_mode_gdt_size - 1;
+        memcpy((void*)PMA_TO_VMA(AP_BOOTSTRAP_PARAM_LONG_MODE_HIGH_MEMORY_GDT), &ap_long_mode_high_memory_gdt_ptr, sizeof(uintptr_t));
 
         // Map the per-core kernel data
         uintptr_t cpu_specific_data_frame = pmm_alloc();
-        printf("Got frame\n");
         // TODO(PT): Use a more direct API that doesn't try to mark the range as allocated, as the range is already allocated via the BSP's VAS
         vas_map_range_exact(ap_vas, CPU_CORE_DATA_BASE, PAGE_SIZE, cpu_specific_data_frame, VAS_RANGE_ACCESS_LEVEL_READ_WRITE, VAS_RANGE_PRIVILEGE_LEVEL_KERNEL);
-        printf("Got mapping\n");
         cpu_core_private_info_t* cpu_core_info = (cpu_core_private_info_t*)PMA_TO_VMA(cpu_specific_data_frame);
         memset(cpu_core_info, 0, sizeof(cpu_core_private_info_t));
         memcpy(&cpu_core_info->processor_info, processor_info, sizeof(processor_info_t));
@@ -157,7 +155,6 @@ void smp_init(void) {
         cpu_core_info->loaded_vas_state = ap_vas;
         cpu_core_info->tss = ap_long_mode_tss;
 
-        printf("\tCalling smp_boot_core...\n");
         smp_boot_core(smp_info, processor_info);
         break;
     }
