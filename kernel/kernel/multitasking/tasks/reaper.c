046c36c78e2d952abca639a29aaf8d52f42d8617
Phillip Tennen <pt@datatheorem.io>
2023-01-24 09:43:50 +0000

    [Kernel] Reaper doesn’t try to iterate/modify linked list of tasks

--- kernel/kernel/multitasking/tasks/reaper.c
+++ kernel/kernel/multitasking/tasks/reaper.c
@@ -4,6 +4,9 @@
 
 #include <kernel/util/spinlock/spinlock.h>
 #include <kernel/util/amc/amc.h>
+#include <kernel/util/amc/amc_internal.h>
+#include <kernel/assert.h>
+#include <kernel/kernel.h>
 
 void reaper_task(void) {
     amc_register_service("com.axle.reaper");
@@ -20,37 +23,25 @@ void reaper_task(void) {
             printf("Reaper ignoring message from [%s]\n", msg->source);
             continue;
         }
+        // TODO(PT): Stop using the `next` field as it'll no longer be a well-formed linked list
         task_small_t** buf = (task_small_t**)msg->body;
         task_small_t* zombie_task = buf[0];
 
         spinlock_acquire(&reaper_lock);
 
-        task_small_t* iter = _tasking_get_linked_list_head();
-        task_small_t* prev = NULL;
-        for (int i = 0; i < MAX_TASKS; i++) {
-            if (iter == NULL) {
-                assert(false, "Reaper failed to find provided task");
-                return;
-            }
+        assert(zombie_task->blocked_info.status == ZOMBIE, "Status was not zombie");
+        printf("Reaper freeing corpse [%d %s]\n", zombie_task->id, zombie_task->name);
 
-            task_small_t* next = iter->next;
-            if (iter == zombie_task) {
-                assert(iter->blocked_info.status == ZOMBIE, "Status was not zombie");
-                printf("Reaper freeing corpse [%d %s]\n", zombie_task->id, zombie_task->name);
-                _thread_destroy(iter);
+        _thread_destroy(zombie_task);
 
-                // Remove this node from the linked-list of tasks
-                if (prev != NULL) {
-                    prev->next = next;
-                }
-                else {
-                    _tasking_set_linked_list_head(next);
-                }
-                break;
-            }
-            prev = iter;
-            iter = (iter)->next;
-        }
+        /*
+        // We need to relaunch tasks here to avoid a race condition.
+        // If we tried to relaunch a task before the call to _thread_destroy(),
+        // the new task might be scheduled before the old task's amc service is cleaned up.
+        amc_service_t* zombie_task_amc_service = amc_service_of_task(zombie_task);
+        // TODO(PT): Have a lookaside table. If the file server crashes more than a few times, fatal OS error
+        bool should_relaunch_task = zombie_task_amc_service && should_relaunch_crashed_amc_service(zombie_task_amc_service);
+        */
 
         spinlock_release(&reaper_lock);
     }
