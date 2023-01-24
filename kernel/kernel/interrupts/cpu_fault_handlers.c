f77473727d6237962f58ac9d17a59fc3390e70f3
Phillip Tennen <pt@datatheorem.io>
2023-01-24 09:34:50 +0000

    [Kernel] CPU fault handlers use modern output API

--- kernel/kernel/interrupts/cpu_fault_handlers.c
+++ kernel/kernel/interrupts/cpu_fault_handlers.c
@@ -14,44 +14,44 @@ static void common_halt(register_state_t* regs, bool recoverable) {
 }
 
 void interrupt_handle_divide_by_zero(register_state_t* regs) {
-	printf_err("Attempted division by zero");
+	printf("Attempted division by zero\n");
 	common_halt(regs, false);
 }
 
 void interrupt_handle_bound_range_exceeded(register_state_t* regs) {
-	printf_err("Bound range exception");
+	printf("Bound range exception\n");
 	common_halt(regs, false);
 }
 
 void interrupt_handle_invalid_opcode(register_state_t* regs) {
-	printf_err("Invalid opcode encountered");
+	printf("Invalid opcode encountered\n");
 	common_halt(regs, false);
 }
 
 void interrupt_handle_device_not_available(register_state_t* regs) {
-	printf_err("Device not available");
+	printf("Device not available\n");
 	common_halt(regs, false);
 }
 
 void interrupt_handle_double_fault(register_state_t* regs) {
-	printf_err("=======================");
-	printf_err("Caught double fault!");
-	printf_err("=======================");
+	printf("=======================\n");
+	printf("Caught double fault\n");
+	printf("=======================\n");
 	common_halt(regs, false);
 }
 
 void interrupt_handle_invalid_tss(register_state_t* regs) {
-	printf_err("Invalid TSS section!");
+	printf("Invalid TSS section\n");
 	common_halt(regs, false);
 }
 
 void interrupt_handle_segment_not_present(register_state_t* regs) {
-	printf_err("Segment not present");
+	printf("Segment not present\n");
 	common_halt(regs, false);
 }
 
 void interrupt_handle_stack_segment_fault(register_state_t* regs) {
-	printf_err("Stack segment fault");
+	printf("Stack segment fault\n");
 	common_halt(regs, false);
 }
 
@@ -61,21 +61,21 @@ void interrupt_handle_general_protection_fault(register_state_t* regs) {
 }
 
 void interrupt_handle_floating_point_exception(register_state_t* regs) {
-	printf_err("Floating point exception");
+	printf("Floating point exception\n");
 	common_halt(regs, false);
 }
 
 void interrupt_handle_alignment_check(register_state_t* regs) {
-	printf_err("Alignment check");
+	printf("Alignment check\n");
 	common_halt(regs, false);
 }
 
 void interrupt_handle_machine_check(register_state_t* regs) {
-	printf_err("Machine check");
+	printf("Machine check\n");
 	common_halt(regs, false);
 }
 
 void interrupt_handle_virtualization_exception(register_state_t* regs) {
-	printf_err("Virtualization exception");
+	printf("Virtualization exception\n");
 	common_halt(regs, false);
 }
