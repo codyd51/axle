d38e9b2a381bd2bb1c7d5a554f4da969c7226271
Phillip Tennen <pt@datatheorem.io>
2023-02-22 22:06:33 +0000

    [TTF] Hinting VM models more instructions such as dropout control

--- rust_programs/ttf_renderer/src/hints.rs
+++ rust_programs/ttf_renderer/src/hints.rs
@@ -1,9 +1,10 @@
 use crate::parser::FontParser;
-use crate::{println, Font};
+use crate::{print, println, Font};
 use agx_definitions::Size;
 use alloc::collections::VecDeque;
 use alloc::vec;
 use alloc::vec::Vec;
+use num_traits::One;
 
 #[derive(Debug, Copy, Clone)]
 enum Axis {
@@ -130,6 +131,94 @@ impl RoundState {
     }
 }
 
+#[derive(Debug, Copy, Clone)]
+enum DropoutControlFlag {
+    UseDropoutControl,
+    DoNotUseDropoutControl,
+}
+
+impl DropoutControlFlag {
+    fn from_scanctrl_word(ppem: usize, low_scanctrl_byte: u8, high_scanctrl_byte: u8) -> Self {
+        let threshold = low_scanctrl_byte as usize;
+        // First, handle the bits that disable scan control
+        let mut should_disable = false;
+        if high_scanctrl_byte & (1 << 3) != 0 {
+            // > Disable dropout_control unless ppem is less than or equal to the threshold value.
+            if ppem > threshold {
+                should_disable = true;
+            }
+        }
+        if high_scanctrl_byte & (1 << 4) != 0 {
+            // > Disable dropout_control unless the glyph is rotated
+            // TODO(PT): No handling now for rotation
+            should_disable = true;
+        }
+        if high_scanctrl_byte & (1 << 5) != 0 {
+            // > Disable dropout_control unless the glyph is stretched
+            // TODO(PT): No handling now for stretching
+            should_disable = true;
+        }
+        // Now handle bits that enable scan control
+        let mut should_enable = false;
+        if high_scanctrl_byte & (1 << 0) != 0 {
+            // > Enable dropout_control if other conditions do not block and
+            // ppem is less than or equal to the threshold value.
+            if ppem <= threshold {
+                should_enable = true;
+            }
+        }
+        if high_scanctrl_byte & (1 << 1) != 0 {
+            // > Enable dropout_control if other conditions do not block and
+            // the glyph is rotated
+            should_enable = true;
+        }
+        if high_scanctrl_byte & (1 << 2) != 0 {
+            // > Enable dropout_control if other conditions do not block and
+            // the glyph is scaled
+            should_enable = true;
+        }
+        let dropout_control_enabled = should_enable && !should_disable;
+        match dropout_control_enabled {
+            true => DropoutControlFlag::UseDropoutControl,
+            false => DropoutControlFlag::DoNotUseDropoutControl,
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+enum DropoutControlMode {
+    DropoutIncludeStubs,
+    DropoutExcludeStubs,
+    FastScanConversion,
+}
+
+impl From<u32> for DropoutControlMode {
+    fn from(value: u32) -> Self {
+        match value {
+            0 => DropoutControlMode::DropoutIncludeStubs,
+            1 => DropoutControlMode::DropoutExcludeStubs,
+            2 => DropoutControlMode::FastScanConversion,
+            _ => panic!("Invalid value"),
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+struct ScanControl {
+    dropout_control_flag: DropoutControlFlag,
+    dropout_control_mode: DropoutControlMode,
+}
+
+impl ScanControl {
+    fn new() -> Self {
+        Self {
+            dropout_control_flag: DropoutControlFlag::DoNotUseDropoutControl,
+            dropout_control_mode: DropoutControlMode::FastScanConversion,
+        }
+    }
+}
+
+#[derive(Debug)]
 pub(crate) struct GraphicsState {
     font_size: Size,
     freedom_vector: Axis,
@@ -137,6 +226,7 @@ pub(crate) struct GraphicsState {
     interpreter_stack: VecDeque<u32>,
     zone_pointers: [Zone; 3],
     round_state: RoundState,
+    scan_control: ScanControl,
 }
 
 impl GraphicsState {
@@ -152,6 +242,7 @@ impl GraphicsState {
                 RoundStatePhase::Zero,
                 RoundStateThreshold::FourEighthsTimesPeriod,
             ),
+            scan_control: ScanControl::new(),
         }
     }
 
@@ -272,6 +363,7 @@ pub(crate) fn parse_instructions(
     graphics_state: &mut GraphicsState,
 ) {
     let mut cursor = 0;
+    let mut last_if_condition_passed: Option<bool> = None;
     loop {
         let opcode: &u8 = FontParser::read_data_with_cursor(instructions, &mut cursor);
         if operations.should_print() {
@@ -303,6 +395,31 @@ pub(crate) fn parse_instructions(
                 }
                 if operations.should_execute() {
                     graphics_state.zone_pointers[0] = zone;
+                }
+            }
+            0x1b => {
+                // ELSE
+                // If we took the IF branch, skip to EIF
+                let take_else = !last_if_condition_passed.unwrap();
+                if operations.should_print() {
+                    let take_else_str = match take_else {
+                        true => "(taken)",
+                        false => "(not taken)",
+                    };
+                    println!("ELSE {}", take_else_str);
+                }
+                if operations.should_execute() {
+                    if !take_else {
+                        // Skip to the next EIF
+                        loop {
+                            let opcode: &u8 =
+                                FontParser::read_data_with_cursor(instructions, &mut cursor);
+                            // EIF opcode
+                            if *opcode == 0x59 {
+                                break;
+                            }
+                        }
+                    }
                 }
             }
             0x23 => {
@@ -344,6 +461,12 @@ pub(crate) fn parse_instructions(
                     //
                 }
             }
+            0x2d => {
+                // ENDF
+                if operations.should_print() {
+                    println!("ENDF");
+                }
+            }
             0x4b => {
                 // Measure pixels per em in the projection vector's axis
                 if operations.should_print() {
@@ -380,14 +503,22 @@ pub(crate) fn parse_instructions(
                     1 => true,
                     _ => panic!("Invalid conditional flag"),
                 };
+                last_if_condition_passed = Some(condition_passed);
 
                 if operations.should_print() {
                     println!("IF\t{condition_passed}");
                 }
                 if operations.should_execute() {
                     if !condition_passed {
-                        // Skip to the next ELSE
-                        todo!()
+                        // Skip to the next ELSE/EIF
+                        loop {
+                            let opcode: &u8 =
+                                FontParser::read_data_with_cursor(instructions, &mut cursor);
+                            // ELSE opcode / EIF opcode
+                            if *opcode == 0x1b || *opcode == 0x59 {
+                                break;
+                            }
+                        }
                     }
                     // Otherwise, if the condition passed, drop through to the next instruction
                 }
@@ -396,6 +527,17 @@ pub(crate) fn parse_instructions(
                 // Nothing to do
                 if operations.should_print() {
                     println!("EIF");
+                }
+            }
+            0x5c => {
+                // NOT
+                let val = graphics_state.pop();
+                if operations.should_print() {
+                    println!("NOT {val:08x}");
+                }
+                if operations.should_execute() {
+                    let result = if val != 0 { 0 } else { 1 };
+                    graphics_state.push(result);
                 }
             }
             0x76 => {
@@ -419,6 +561,61 @@ pub(crate) fn parse_instructions(
                 }
                 //
             }
+            0x77 => {
+                println!("TODO: Super round @ 45 degrees");
+                let val = graphics_state.pop();
+            }
+            0x85 => {
+                // SCANCTRL Scan conversion control
+                let word = graphics_state.pop();
+                // Lower byte represents threshold value for PPEM
+                let low = word & 0xff;
+                let high = (word >> 8) & 0xff;
+                if operations.should_print() {
+                    println!("SCANCTRL {low:04x} : {high:04x}");
+                }
+                if operations.should_execute() {
+                    // TODO(PT): Which axis should this use?
+                    graphics_state.scan_control.dropout_control_flag =
+                        DropoutControlFlag::from_scanctrl_word(
+                            graphics_state.font_size.width as _,
+                            low as _,
+                            high as _,
+                        );
+                }
+            }
+            0x88 => {
+                // GETINFO
+                let selector = graphics_state.pop();
+                if operations.should_print() {
+                    println!("GETINFO");
+                }
+                let mut result = 0_u32;
+                if operations.should_execute() {
+                    if selector & (1 << 0) != 0 {
+                        // Engine version
+                        result |= 0xcafebabe;
+                    }
+                    if selector & (1 << 1) != 0 {
+                        // Rotated = false
+                    }
+                    if selector & (1 << 2) != 0 {
+                        // Stretched = false
+                    }
+                    graphics_state.push(result);
+                }
+            }
+            0x8d => {
+                // SCANTYPE
+                let word = graphics_state.pop();
+                if operations.should_print() {
+                    println!("SCANTYPE {word:08x}");
+                }
+                if operations.should_execute() {
+                    graphics_state.scan_control.dropout_control_mode =
+                        DropoutControlMode::from(word);
+                }
+            }
             0xb0..=0xb7 => {
                 // PUSH bytes
                 let number_of_bytes_to_push = 1 + (opcode - 0xb0);
@@ -437,6 +634,30 @@ pub(crate) fn parse_instructions(
                 if operations.should_execute() {
                     for byte in bytes_to_push.iter() {
                         graphics_state.push(*byte as u32);
+                    }
+                }
+            }
+            0xb8..=0xbf => {
+                // PUSH words
+                let number_of_words_to_push = 1 + (opcode - 0xb8);
+                let mut words_to_push: Vec<u16> = vec![];
+                for _ in 0..number_of_words_to_push {
+                    let word_bytes =
+                        FontParser::read_bytes_from_data_with_cursor(instructions, &mut cursor, 2);
+                    // The high byte is popped first
+                    let word = (word_bytes[0] as u16) << 8 | word_bytes[1] as u16;
+                    words_to_push.push(word);
+                }
+                if operations.should_print() {
+                    print!("Push {number_of_words_to_push} words:");
+                    for word in words_to_push.iter() {
+                        print!("  {word:02x}");
+                    }
+                    println!();
+                }
+                if operations.should_execute() {
+                    for word in words_to_push.iter() {
+                        graphics_state.push(*word as u32);
                     }
                 }
             }
