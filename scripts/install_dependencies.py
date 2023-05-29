42187ae0780b78b58e939daa3a4d35890799cbc2
Phillip Tennen <pt@datatheorem.io>
2023-05-29 21:48:23 +0100

    [Meta] Update installation script to provide a path for macOS hosts

--- scripts/install_dependencies.py
+++ scripts/install_dependencies.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-from pathlib import Path
+import sys
 
 from build_utils import run_and_check
 
 
-def install_dependencies():
-    print(f"Installing dependencies...")
+def install_dependencies_on_linux():
     run_and_check(["sudo", "apt", "update"])
     dependencies = [
         # Toolchain
@@ -27,8 +26,28 @@ def install_dependencies():
     # run_and_check(["pip3", "install",  "-r", requirements_file.as_posix()])
 
 
+def install_dependencies_on_macos():
+    dependencies = [
+        # Toolchain
+        "bison",
+        "gmp",
+        "mpc",
+        "libmpc",
+        "mpfr",
+        "xorriso",
+        "autoconf@2.69",
+        # OS build
+        "mtools",
+    ]
+    run_and_check(["brew", "install", *dependencies])
+
+
 def main() -> None:
-    install_dependencies()
+    print(f"Installing dependencies...")
+    if sys.platform == 'darwin':
+        install_dependencies_on_macos()
+    else:
+        install_dependencies_on_linux()
 
 
 if __name__ == '__main__':
